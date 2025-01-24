e731a19e0bc24cb1c6de156ce0829bebca981d9b
Ryan O'Neill <ryanoneill@gmail.com>
2025-01-24 09:45:32 -0800

    Added Running Sum of 1D Array

---
+++ running_sum_of_1d_array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = []
+        sum = 0
+        for num in nums:
+            sum += num
+            result.append(sum)
+        return result
---
+++ test_running_sum_of_1d_array.py
@@ -0,0 +1,19 @@
+from running_sum_of_1d_array import Solution
+
+def test_ex1():
+    nums = [1,2,3,4]
+    solution = Solution()
+    result = solution.runningSum(nums)
+    assert result == [1,3,6,10]
+
+def test_ex2():
+    nums = [1,1,1,1,1]
+    solution = Solution()
+    result = solution.runningSum(nums)
+    assert result == [1,2,3,4,5]
+
+def test_ex3():
+    nums = [3,1,2,10,1]
+    solution = Solution()
+    result = solution.runningSum(nums)
+    assert result == [3,4,6,16,17]
