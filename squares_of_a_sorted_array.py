4872223b80280fa2471fbd78974f4453cce63d7d
Ryan O'Neill <ryanoneill@gmail.com>
2025-01-23 19:11:23 -0800

    Added Squares of a Sorted Array

---
+++ squares_of_a_sorted_array.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        right = 0
+        while nums[right] < 0:
+            right += 1
+        left = right - 1
+        result = []
+        n = len(nums)
+        while left > -1 or right < n:
+            if left == -1:
+                value = nums[right]
+                value = value * value
+                result.append(value)
+                right += 1
+            elif right == n:
+                value = nums[left]
+                value = value * value
+                result.append(value)
+                left -= 1
+            else:
+                left_value = nums[left]
+                left_value = left_value * left_value
+                right_value = nums[right]
+                right_value = right_value * right_value
+                if left_value < right_value:
+                    result.append(left_value)
+                    left -= 1
+                else:
+                    result.append(right_value)
+                    right += 1
+        return result
+
+
---
+++ test_squares_of_a_sorted_array.py
@@ -0,0 +1,13 @@
+from squares_of_a_sorted_array import Solution
+
+def test_ex1():
+    nums = [-4,-1,0,3,10]
+    solution = Solution()
+    result = solution.sortedSquares(nums)
+    assert result == [0,1,9,16,100]
+
+def test_ex2():
+    nums = [-7,-3,2,3,11]
+    solution = Solution()
+    result = solution.sortedSquares(nums)
+    assert result == [4,9,9,49,121]
