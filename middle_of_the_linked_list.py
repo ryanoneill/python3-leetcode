4667d76e0faae802c990865d8ed9b66b8827fb0e
Ryan O'Neill <ryanoneill@gmail.com>
2025-01-25 13:31:16 -0800

    Added Middle of the Linked List

---
+++ middle_of_the_linked_list.py
@@ -0,0 +1,15 @@
+from list_node import ListNode
+from typing import Optional
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+
+        while fast != None:
+            fast = fast.next
+            if fast is not None:
+                fast = fast.next
+                slow = slow.next
+
+        return slow
