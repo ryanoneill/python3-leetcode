b14e0217dfcb05e905a844ca5256c9ec27006b54
Ryan O'Neill <ryanoneill@gmail.com>
2025-01-29 21:26:29 -0800

    Added Diameter of Binary Tree

---
+++ test_diameter_of_binary_tree.py
@@ -0,0 +1,19 @@
+from diameter_of_binary_tree import Solution
+from serialize_and_deserialize_binary_tree import Codec
+from tree_node import TreeNode
+
+def test_ex1():
+    root = "[1,2,3,4,5]"
+    codec = Codec()
+    head = codec.deserialize(root)
+    solution = Solution()
+    result = solution.diameterOfBinaryTree(head)
+    assert result == 3
+
+def test_ex2():
+    root = "[1,2]"
+    codec = Codec()
+    head = codec.deserialize(root)
+    solution = Solution()
+    result = solution.diameterOfBinaryTree(head)
+    assert result == 1
