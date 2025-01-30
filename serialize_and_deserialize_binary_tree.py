7ee1d3e219b714d936cbae3664f4356f905bad34
Ryan O'Neill <ryanoneill@gmail.com>
2025-01-29 20:49:19 -0800

    Added Serialize and Deserialize Binary Tree

---
+++ serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,61 @@
+from queue import deque
+from tree_node import TreeNode
+from typing import Optional
+
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        result = []
+
+        items = deque()
+        items.append(root)
+
+        while items:
+            n = len(items)
+            for _ in range(n):
+                item = items.popleft()
+                if item:
+                    result.append(str(item.val))
+                    items.append(item.left)
+                    items.append(item.right)
+                else:
+                    result.append("null")
+
+        while result and result[-1] == "null":
+            result.pop()
+
+        return "[" + ",".join(result) + "]"
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if data == "[]":
+            result = None
+        else:
+            result = None
+            queue = deque()
+            items = data[1:-1].split(",")
+            n = len(items)
+            i = 0
+
+            while i < n:
+                parent = None
+                if queue:
+                    parent = queue.popleft()
+                if parent:
+                    left = items[i]
+                    if left != "null":
+                        parent.left = TreeNode(left)
+                        queue.append(parent.left)
+                    i += 1
+                    if i < n:
+                        right = items[i]
+                        i += 1
+                        if right != "null":
+                            parent.right = TreeNode(right)
+                            queue.append(parent.right)
+                else:
+                    root = TreeNode(items[i])
+                    i += 1
+                    result = root
+                    queue.append(root)
+
+        return result
---
+++ test_serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,50 @@
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    root = "[1,2,3,null,null,4,5]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
+
+def test_ex2():
+    root = "[]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
+
+def test_ex3():
+    root = "[1,2,3,4,5,6,7,8]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
+
+def test_ex4():
+    root = "[2,null,3,null,4,null,5,null,6]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
+
+def test_ex5():
+    root = "[5,4,8,11,null,13,4,7,2,null,null,null,1]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
+
+def test_ex6():
+    root = "[1,2]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
+
+def test_ex7():
+    root = "[1,-2,-3,1,3,-2,null,-1]"
+    codec = Codec()
+    result = codec.deserialize(root)
+    result = codec.serialize(result)
+    assert result == root
