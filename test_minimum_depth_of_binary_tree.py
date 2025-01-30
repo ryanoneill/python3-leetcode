9bd8d249c41dd393980869ea8d7b4decaa3057f2
Ryan O'Neill <ryanoneill@gmail.com>
2025-01-29 20:51:31 -0800

    Updated Minimum Depth of Binary Tree

--- test_minimum_depth_of_binary_tree.py
+++ test_minimum_depth_of_binary_tree.py
@@ -1,33 +1,20 @@
 from tree_node import TreeNode
 from minimum_depth_of_binary_tree import Solution
-
-# TODO: implement Codec for serialization and deserialization of binary tree
+from serialize_and_deserialize_binary_tree import Codec
 
 def test_ex1():
-    head = TreeNode(3)
-    nine = TreeNode(9)
-    head.left = nine
-    twenty = TreeNode(20)
-    head.right = twenty
-    fifteen = TreeNode(15)
-    twenty.left = fifteen
-    seven = TreeNode(7)
-    twenty.right = seven
+    root = "[3,9,20,null,null,15,7]"
+    codec = Codec()
+    head = codec.deserialize(root)
 
     solution = Solution()
     result = solution.minDepth(head)
     assert result == 2
 
 def test_ex2():
-    head = TreeNode(2)
-    three = TreeNode(3)
-    head.right = three
-    four = TreeNode(4)
-    three.right = four
-    five = TreeNode(5)
-    four.right = five
-    six = TreeNode(6)
-    five.right = six
+    root = "[2,null,3,null,4,null,5,null,6]"
+    codec = Codec()
+    head = codec.deserialize(root)
 
     solution = Solution()
     result = solution.minDepth(head)
