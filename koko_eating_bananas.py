f93a39e5da594c15fd32dfaa80a694f35afed289
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-01 13:54:52 -0800

    Added Koko Eating Bananas

---
+++ koko_eating_bananas.py
@@ -0,0 +1,22 @@
+from typing import List
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+
+        while left < right:
+            middle = left + ((right - left) // 2)
+
+            spent = 0
+            for pile in piles:
+                spent += math.ceil(pile / middle)
+
+            possible = spent <= h
+            if possible:
+                right = middle
+            else:
+                left = middle + 1
+
+        return left
