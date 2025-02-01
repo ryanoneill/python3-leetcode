47f32bbaea317bf9b3a2baa84c26aee9b27e1944
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-01 13:19:16 -0800

    Added Find Peak Element

---
+++ test_find_peak_element.py
@@ -0,0 +1,13 @@
+from find_peak_element import Solution
+
+def test_ex1():
+    nums = [1,2,3,1]
+    solution = Solution()
+    result = solution.findPeakElement(nums)
+    assert result == 2
+
+def test_ex2():
+    nums = [1,2,1,3,5,6,4]
+    solution = Solution()
+    result = solution.findPeakElement(nums)
+    assert result == 5
