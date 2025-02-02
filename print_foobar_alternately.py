6ec2f400cf78b3c70ababf5f5ddc8db2abb4a1c0
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-02 13:27:00 -0800

    Added Print Foobar Alternately

---
+++ print_foobar_alternately.py
@@ -0,0 +1,24 @@
+from threading import Condition
+from typing import Callable
+
+class FooBar:
+    def __init__(self, n):
+        self.foo_ready = True
+        self.foo_condition = Condition()
+        self.n = n
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            with self.foo_condition:
+                self.foo_condition.wait_for(lambda: self.foo_ready)
+                printFoo()
+                self.foo_ready = False
+                self.foo_condition.notify()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            with self.foo_condition:
+                self.foo_condition.wait_for(lambda: not self.foo_ready)
+                printBar()
+                self.foo_ready = True
+                self.foo_condition.notify()
---
+++ test_print_foobar_alternately.py
@@ -0,0 +1,38 @@
+from print_foobar_alternately import FooBar
+from threading import Thread
+
+class FooBarTester:
+    def __init__(self, foobar):
+        self.data = ""
+        self.foobar = foobar
+        self.t_foo = Thread(target=foobar.foo, args=[self.printFoo])
+        self.t_bar = Thread(target=foobar.bar, args=[self.printBar])
+
+    def printFoo(self):
+        self.data += "foo"
+
+    def printBar(self):
+        self.data += "bar"
+
+    def run(self) -> str:
+        self.t_bar.start()
+        self.t_foo.start()
+
+        self.t_bar.join()
+        self.t_foo.join()
+
+        return self.data
+
+def test_ex1():
+    n = 1
+    foobar = FooBar(n)
+    fbt = FooBarTester(foobar)
+    result = fbt.run()
+    assert result == "foobar"
+
+def test_ex2():
+    n = 2
+    foobar = FooBar(n)
+    fbt = FooBarTester(foobar)
+    result = fbt.run()
+    assert result == "foobarfoobar"
