3d531ff24656b949d771ca38d2bbbf5b7f730e6d
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-02 12:35:39 -0800

    Added Print in Order

---
+++ print_in_order.py
@@ -0,0 +1,31 @@
+from typing import Callable
+import threading
+
+class Foo:
+    def __init__(self):
+        self.first_done = False
+        self.first_condition = threading.Condition()
+
+        self.second_done = False
+        self.second_condition = threading.Condition()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        printFirst()
+        with self.first_condition:
+            self.first_done = True
+            self.first_condition.notify()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        while not self.first_done:
+            with self.first_condition:
+                self.first_condition.wait()
+        printSecond()
+        with self.second_condition:
+            self.second_done = True
+            self.second_condition.notify()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        while not self.second_done:
+            with self.second_condition:
+                self.second_condition.wait()
+        printThird()
---
+++ test_print_in_order.py
@@ -0,0 +1,61 @@
+from typing import List
+from print_in_order import Foo
+from threading import Thread
+
+class PrintTester:
+    def __init__(self, foo):
+        self.data = ""
+        self.foo = foo
+        self.t1 = Thread(target=foo.first(self.printFirst))
+        self.t2 = Thread(target=foo.second(self.printSecond))
+        self.t3 = Thread(target=foo.third(self.printThird))
+
+    def printFirst(self):
+        self.data += "first"
+
+    def printSecond(self):
+        self.data += "second"
+
+    def printThird(self):
+        self.data += "third"
+
+    def getResults(self) -> str:
+        return self.data
+
+    def run(self, nums: List[int]) -> str:
+        for num in nums:
+            if num == 1:
+                self.t1.start()
+            elif num == 2:
+                self.t2.start()
+            elif num == 3:
+                self.t3.start()
+            else:
+                raise f'unknown num {num}'
+
+        self.t1.join()
+        self.t2.join()
+        self.t3.join()
+
+        return self.getResults()
+
+def test_ex1():
+    nums = [1,2,3]
+    foo = Foo()
+    pt = PrintTester(foo)
+    result = pt.run(nums)
+    assert result == "firstsecondthird"
+
+def test_ex2():
+    nums = [1,3,2]
+    foo = Foo()
+    pt = PrintTester(foo)
+    result = pt.run(nums)
+    assert result == "firstsecondthird"
+
+def test_ex3():
+    nums = [3,2,1]
+    foo = Foo()
+    pt = PrintTester(foo)
+    result = pt.run(nums)
+    assert result == "firstsecondthird"
