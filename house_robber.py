2d8c995f9cddfdff31986909ce2ca902e897aa84
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-04 12:27:08 -0800

    Added House Robber

---
+++ house_robber.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        result = 0
+        n = len(nums)
+
+        if n == 0:
+            result = 0
+        elif n == 1:
+            result = nums[0]
+        else:
+            dp = [0] * n
+
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                yes = nums[i] + dp[i-2]
+                no = dp[i-1]
+                dp[i] = max(yes, no)
+
+            result = dp[-1]
+
+        return result
---
+++ test_house_robber.py
@@ -0,0 +1,19 @@
+from house_robber import Solution
+
+def test_ex1():
+    nums = [1,2,3,1]
+    solution = Solution()
+    result = solution.rob(nums)
+    assert result == 4
+
+def test_ex2():
+    nums = [2,7,9,3,1]
+    solution = Solution()
+    result = solution.rob(nums)
+    assert result == 12
+
+def test_ex3():
+    nums = [5]
+    solution = Solution()
+    result = solution.rob(nums)
+    assert result == 5
