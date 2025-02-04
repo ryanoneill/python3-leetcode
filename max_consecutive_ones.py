3cde1da0c38a01de3ec9303933902dedd424986f
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-04 11:48:11 -0800

    Added Max Consecutive Ones

---
+++ max_consecutive_ones.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        result = 0
+        current = 0
+        for num in nums:
+            if num == 1:
+                current += 1
+                result = max(current, result)
+            else:
+                current = 0
+        return result
---
+++ test_max_consecutive_ones.py
@@ -0,0 +1,13 @@
+from max_consecutive_ones import Solution
+
+def test_ex1():
+    nums = [1,1,0,1,1,1]
+    solution = Solution()
+    result = solution.findMaxConsecutiveOnes(nums)
+    assert result == 3
+
+def test_ex2():
+    nums = [1,0,1,1,0,1]
+    solution = Solution()
+    result = solution.findMaxConsecutiveOnes(nums)
+    assert result == 2
