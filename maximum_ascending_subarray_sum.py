33f293d414e7c66ba350a4de73241460abbb4e03
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-03 21:00:07 -0800

    Added Maximum Ascending Subarray Sum

---
+++ maximum_ascending_subarray_sum.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        last = nums[0]
+        current = nums[0]
+        result = current
+
+        for i in range(1, n):
+            num = nums[i]
+            if num > last:
+                current += num
+                last = num
+                result = max(result, current)
+            else:
+                current = num
+                last = num
+                result = max(result, current)
+
+        return result
---
+++ test_maximum_ascending_subarray.py
@@ -0,0 +1,19 @@
+from maximum_ascending_subarray_sum import Solution
+
+def test_ex1():
+    nums = [10,20,30,5,10,50]
+    solution = Solution()
+    result = solution.maxAscendingSum(nums)
+    assert result == 65
+
+def test_ex2():
+    nums = [10,20,30,40,50]
+    solution = Solution()
+    result = solution.maxAscendingSum(nums)
+    assert result == 150
+
+def test_ex3():
+    nums = [12,17,15,13,10,11,12]
+    solution = Solution()
+    result = solution.maxAscendingSum(nums)
+    assert result == 33
