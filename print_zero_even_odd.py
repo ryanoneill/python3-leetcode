3694927787c80232a9673fdee0a2dfaa972ab8c0
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-03 20:49:13 -0800

    Added Print Zero Even Odd

---
+++ print_zero_even_odd.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from typing import Callable
+from threading import Condition
+
+class State(Enum):
+    ZERO = 1
+    ODD = 2
+    EVEN = 3
+
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+        self.state = State.ZERO
+        self.state_condition = Condition()
+
+    def zero(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n + 1):
+            with self.state_condition:
+                self.state_condition.wait_for(lambda: self.state == State.ZERO)
+                printNumber(0)
+                if i % 2 == 1:
+                    self.state = State.ODD
+                else:
+                    self.state = State.EVEN
+                self.state_condition.notify_all()
+
+    def even(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n + 1):
+            if i % 2 == 0:
+                with self.state_condition:
+                    self.state_condition.wait_for(lambda: self.state == State.EVEN)
+                    printNumber(i)
+                    self.state = State.ZERO
+                    self.state_condition.notify_all()
+
+    def odd(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n + 1):
+            if i % 2 == 1:
+                with self.state_condition:
+                    self.state_condition.wait_for(lambda: self.state == State.ODD)
+                    printNumber(i)
+                    self.state = State.ZERO
+                    self.state_condition.notify_all()
---
+++ test_print_zero_even_odd.py
@@ -0,0 +1,39 @@
+from print_zero_even_odd import ZeroEvenOdd
+from threading import Thread
+
+class ZeroEvenOddTester:
+    def __init__(self, zeo):
+        self.data = ""
+        self.zeo = zeo
+        self.zero_thread = Thread(target=zeo.zero, args=[self.printNumber])
+        self.odd_thread = Thread(target=zeo.odd, args=[self.printNumber])
+        self.even_thread = Thread(target=zeo.even, args=[self.printNumber])
+
+    def printNumber(self, value: int):
+        self.data += str(value)
+
+    def run(self):
+        self.zero_thread.start()
+        self.odd_thread.start()
+        self.even_thread.start()
+
+        self.zero_thread.join()
+        self.odd_thread.join()
+        self.even_thread.join()
+
+        return self.data
+
+def test_ex1():
+    n = 2
+    zeo = ZeroEvenOdd(n)
+    zeot = ZeroEvenOddTester(zeo)
+    result = zeot.run()
+    assert result == "0102"
+
+def test_ex2():
+    n = 5
+    zeo = ZeroEvenOdd(n)
+    zeot = ZeroEvenOddTester(zeo)
+    result = zeot.run()
+    assert result == "0102030405"
+
