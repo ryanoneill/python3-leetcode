08b18596d75e1e46c7b34fd2ff02e08357c239a8
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-04 17:31:05 -0800

    Added Check if One String Swap Can Make Strings Equal

---
+++ check_if_one_string_swap_can_make_strings_equal.py
@@ -0,0 +1,33 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+
+        if n1 == n2:
+            result = True
+            diffs = []
+            for i in range(n1):
+                c1 = s1[i]
+                c2 = s2[i]
+                if c1 != c2:
+                    diffs.append(i)
+                    if len(diffs) > 2:
+                        break
+
+
+        d = len(diffs)
+        if d == 0:
+            result = True
+        elif d == 1:
+            result = False
+        elif d == 2:
+            i = diffs.pop()
+            j = diffs.pop()
+            result = s1[i] == s2[j] and s1[j] == s2[i]
+        else:
+            result = False
+
+        return result
+        
+
+
---
+++ test_check_if_one_string_swap_can_make_strings_equal.py
@@ -0,0 +1,21 @@
+from check_if_one_string_swap_can_make_strings_equal import Solution
+
+def test_ex1():
+    s1 = "bank"
+    s2 = "kanb"
+    solution = Solution()
+    result = solution.areAlmostEqual(s1, s2)
+
+def test_ex2():
+    s1 = "attack"
+    s2 = "defend"
+    solution = Solution()
+    result = solution.areAlmostEqual(s1, s2)
+    assert not result
+
+def test_ex3():
+    s1 = "kelb"
+    s2 = "kelb"
+    solution = Solution()
+    result = solution.areAlmostEqual(s1, s2)
+    assert result
