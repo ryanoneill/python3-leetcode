0c8794b16a6e7bcfee894943c5cf03d89b39de4e
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-08 16:29:32 -0800

    Added Count Number of Bad Pairs

---
+++ count_number_of_bad_pairs.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    # j - i != nums[j] - nums[i]
+    # is the same as
+    # nums[i] - i != nums[j] - j
+    def countBadPairs(self, nums: List[int]) -> int:
+        result = 0
+        counts = {}
+        n = len(nums)
+        for i in range(n):
+            diff = nums[i] - i
+            if diff in counts:
+                counts[diff] += 1
+            else:
+                counts[diff] = 1
+            result += (i + 1) - counts[diff]
+        return result
---
+++ test_count_number_of_bad_pairs.py
@@ -0,0 +1,13 @@
+from count_number_of_bad_pairs import Solution
+
+def test_ex1():
+    nums = [4,1,3,3]
+    solution = Solution()
+    result = solution.countBadPairs(nums)
+    assert result == 5
+
+def test_ex2():
+    nums = [1,2,3,4,5]
+    solution = Solution()
+    result = solution.countBadPairs(nums)
+    assert result == 0
