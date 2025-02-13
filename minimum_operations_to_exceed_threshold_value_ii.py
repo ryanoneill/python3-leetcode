dc8d78ee6311385fd2f7e10f2bf6b1785ffee2b7
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-12 18:03:41 -0800

    Added Minimum Operations to Exceed Threshold Value II

---
+++ minimum_operations_to_exceed_threshold_value_ii.py
@@ -0,0 +1,15 @@
+from typing import List
+from heapq import heapify, heappush, heappop
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        result = 0
+        heapify(nums)
+        while len(nums) >= 2 and nums[0] < k:
+            x = heappop(nums)
+            y = heappop(nums)
+            item = min(x, y) * 2 + max(x, y)
+            heappush(nums, item)
+            result += 1
+        return result
+
---
+++ test_minimum_operations_to_exceed_threshold_value_ii.py
@@ -0,0 +1,15 @@
+from minimum_operations_to_exceed_threshold_value_ii import Solution
+
+def test_ex1():
+    nums = [2,11,10,1,3]
+    k = 10
+    solution = Solution()
+    result = solution.minOperations(nums, k)
+    assert result == 2
+
+def test_ex2():
+    nums = [1,1,2,4,9]
+    k = 20
+    solution = Solution()
+    result = solution.minOperations(nums, k)
+    assert result == 4
