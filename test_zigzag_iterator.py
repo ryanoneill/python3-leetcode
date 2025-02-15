87b6dc3a111a8d66bc10da1a57665e1d29f0341b
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-15 15:28:19 -0800

    Added Snapshot Array

---
+++ test_zigzag_iterator.py
@@ -0,0 +1,28 @@
+from zigzag_iterator import ZigzagIterator
+
+def test_ex1():
+    v1 = [1,2]
+    v2 = [3,4,5,6]
+    zi = ZigzagIterator(v1, v2)
+    results = []
+    while zi.hasNext():
+        results.append(zi.next())
+    assert results == [1,3,2,4,5,6]
+
+def test_ex2():
+    v1 = [1]
+    v2 = []
+    zi = ZigzagIterator(v1, v2)
+    results = []
+    while zi.hasNext():
+        results.append(zi.next())
+    assert results == [1]
+
+def test_ex3():
+    v1 = []
+    v2 = [1]
+    zi = ZigzagIterator(v1, v2)
+    results = []
+    while zi.hasNext():
+        results.append(zi.next())
+    assert results == [1]
---
+++ zigzag_iterator.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class ZigzagIterator:
+    def __init__(self, v1: List[int], v2: List[int]):
+        self.v1 = v1
+        self.v2 = v2
+        self.v1_n = len(v1)
+        self.v2_n = len(v2)
+        self.first = self.v1_n > 0
+        self.index = 0
+
+    def next(self) -> int:
+        result = -1 
+        if self.first:
+            result = self.v1[self.index]
+            if self.index < self.v2_n:
+                self.first = False
+            else:
+                self.index += 1
+        else:
+            result = self.v2[self.index]
+            self.index += 1
+            if self.index < self.v1_n:
+                self.first = True
+        return result
+
+    def hasNext(self) -> bool:
+        result = False
+        if self.first:
+            result = self.index < self.v1_n
+        else:
+            result = self.index < self.v2_n
+        return result
