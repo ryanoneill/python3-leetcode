c98c26d240fb17ab0f820c8f66f580fa7c0ed88c
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-14 21:06:06 -0800

    Added Delete N Nodes After M Nodes of a Linked List

---
+++ delete_n_nodes_after_m_nodes_of_a_linked_list.py
@@ -0,0 +1,28 @@
+from list_node import ListNode
+from typing import Optional
+
+class Solution:
+    def deleteNodes(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
+        result = head
+        previous = head
+        current = head
+
+        done = False
+        while not done:
+            for _ in range(m):
+                previous = current
+                current = current.next
+                if current is None:
+                    done = True
+                    break
+            if not done:
+                for _ in range(n):
+                    current = current.next
+                    previous.next = current
+                    if current is None:
+                        done = True
+                        break
+
+        return result
+
+
