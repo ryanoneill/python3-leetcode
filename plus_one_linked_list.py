4f8cab88df2e7ec5833752d1976aec75bca35af3
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-14 22:24:36 -0800

    Added Plus One Linked List

---
+++ plus_one_linked_list.py
@@ -0,0 +1,26 @@
+from list_node import ListNode
+
+class Solution:
+    def plusOne(self, head: ListNode) -> ListNode:
+        result = head
+        stack = []
+
+        current = head
+        while current:
+            stack.append(current)
+            current = current.next
+        carry = True
+        while carry:
+            if stack:
+                item = stack.pop()
+                if item.val == 9:
+                    item.val = 0
+                else:
+                    item.val += 1
+                    carry = False
+            else:
+                result = ListNode(1, head)
+                carry = False
+
+        return result
+
---
+++ test_plus_one_linked_list.py
@@ -0,0 +1,23 @@
+from list_node import from_list, to_list
+from plus_one_linked_list import Solution
+
+def test_ex1():
+    items = [1,2,3]
+    head = from_list(items)
+    solution = Solution()
+    result = solution.plusOne(head)
+    assert to_list(result) == [1,2,4]
+
+def test_ex2():
+    items = [0]
+    head = from_list(items)
+    solution = Solution()
+    result = solution.plusOne(head)
+    assert to_list(result) == [1]
+
+def test_ex3():
+    items = [9]
+    head = from_list(items)
+    solution = Solution()
+    result = solution.plusOne(head)
+    assert to_list(result) == [1,0]
