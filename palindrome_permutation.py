d6da8a31f2811be3a0c63c869b521a2b1df26c47
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-14 18:33:37 -0800

    Added Palindrome Permutation

---
+++ palindrome_permutation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        odd_count = 0
+
+        counts = {}
+        for letter in s:
+            if letter in counts:
+                counts[letter] += 1
+                if counts[letter] % 2 == 0:
+                    odd_count -= 1
+                else:
+                    odd_count += 1
+            else:
+                counts[letter] = 1
+                odd_count += 1
+
+        return odd_count < 2
+
+
+
