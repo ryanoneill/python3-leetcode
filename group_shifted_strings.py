cf5a26d73909787b74a474bd71b3eb83d8294282
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-14 19:15:03 -0800

    Added Group Shifted Strings

---
+++ group_shifted_strings.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def key(self, s: str) -> str:
+        result = []
+        n = len(s)
+        base = ord(s[0])
+        for letter in s:
+            value = (ord(letter) - base + 26) % 26
+            result.append(chr(value + ord('a')))
+        return "".join(result)
+
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        results = []
+        hash = {}
+        for s in strings:
+            key = self.key(s)
+            if key in hash:
+                hash[key].append(s)
+            else:
+                hash[key] = [s]
+
+        for key in hash:
+            results.append(hash[key])
+
+        return results
+
---
+++ test_group_shifted_strings.py
@@ -0,0 +1,14 @@
+from group_shifted_strings import Solution
+
+def test_ex1():
+    strings = ["abc","bcd","acef","xyz","az","ba","a","z"]
+    solution = Solution()
+    results = solution.groupStrings(strings)
+    expected = [["acef"],["a","z"],["abc","bcd","xyz"],["az","ba"]]
+    assert sorted(results) == sorted(expected)
+
+def test_ex2():
+    strings = ["a"] 
+    solution = Solution()
+    results = solution.groupStrings(strings)
+    assert results == [["a"]]
