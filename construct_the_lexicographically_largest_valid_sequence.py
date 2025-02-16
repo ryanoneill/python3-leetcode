1f2807a54879b313524ce6ea03b09cdd03e867b8
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-15 17:45:02 -0800

    Added Construct the Lexicographically Largest Valid Sequence

---
+++ construct_the_lexicographically_largest_valid_sequence.py
@@ -0,0 +1,46 @@
+from typing import List
+from heapq import heappush, heappop
+
+class Solution:
+    def worker(self, items: List[int], remain: List[int]) -> (bool, List[int]):
+        if not remain:
+            return (True, items)
+        else:
+            for i in range(len(remain)):
+                value = remain[i]
+                these = items[:]
+                left = [x for x in remain if x != value]
+                first = these.index(0)
+                these[first] = value
+                if value == 1:
+                    return self.worker(these, left)
+                else:
+                    second = first + value
+                    if second < len(these) and these[second] == 0:
+                        these[second] = value
+                        (result, response) = self.worker(these, left)
+                        if result:
+                            return (result, response)
+        return (False, [])
+
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        length = (n-1) * 2 + 1
+        items = [0 for i in range(length)]
+        remain = [i for i in range(n,0,-1)]
+
+        (result, value) = self.worker(items, remain)
+
+        return value
+
+    # n = 1, result = [1]
+    # n = 2, result = [2, 1, 2]
+    # n = 3, result = [3, 1, 2, 3, 2]
+    # n = 4, result = [4, 2, 3, 2, 4, 3, 1]
+    # n = 5, result = [5, 3, 1, 4, 3, 5, 2, 4, 2]
+
+    # For every integer i between 2 and n, the distance between the
+    # two occurrences of i is exactly i
+
+    # 1 must occur once
+    # 2 must occur twice
+    # 3 must occur twice
---
+++ test_construct_the_lexicographically_largest_valid_sequence.py
@@ -0,0 +1,31 @@
+from construct_the_lexicographically_largest_valid_sequence import Solution
+
+def test_ex1():
+    n = 3
+    solution = Solution()
+    result = solution.constructDistancedSequence(n)
+    assert result == [3, 1, 2, 3, 2]
+
+def test_ex2():
+    n = 5
+    solution = Solution()
+    result = solution.constructDistancedSequence(n)
+    assert result == [5, 3, 1, 4, 3, 5, 2, 4, 2]
+
+def test_ex3():
+    n = 1
+    solution = Solution()
+    result = solution.constructDistancedSequence(n)
+    assert result == [1]
+
+def test_ex4():
+    n = 4
+    solution = Solution()
+    result = solution.constructDistancedSequence(n)
+    assert result == [4, 2, 3, 2, 4, 3, 1]
+
+def test_ex5():
+    n = 2
+    solution = Solution()
+    result = solution.constructDistancedSequence(n)
+    assert result == [2, 1, 2]
