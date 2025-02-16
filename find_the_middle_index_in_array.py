16bc813863d160f9ea95b6e55e8617a3aca37b4f
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-16 15:36:13 -0800

    Added Find the Middle Index in Array

---
+++ find_the_middle_index_in_array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        result = -1
+
+        # O(n) for entire sum
+        total = sum(nums)
+
+        running = 0
+        for i in range(len(nums)):
+            value = nums[i]
+            if running == (total - running - value):
+                result = i
+                break
+            else:
+                running += value
+                
+        return result
