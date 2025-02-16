41887b447101f86abb285f7367c156fcb246f6fe
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-16 15:06:48 -0800

    Added Vertical Order Traversal of a Binary Tree

---
+++ test_vertical_order_traversal_of_a_binary_tree.py
@@ -0,0 +1,18 @@
+from vertical_order_traversal_of_a_binary_tree import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    data = "[3,9,20,null,null,15,7]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    results = solution.verticalTraversal(root)
+    assert results == [[9],[3,15],[20],[7]]
+
+def test_ex2():
+    data = "[1,2,3,4,5,6,7]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    results = solution.verticalTraversal(root)
+    assert results == [[4],[2],[1,5,6],[3],[7]]
---
+++ vertical_order_traversal_of_a_binary_tree.py
@@ -0,0 +1,31 @@
+from tree_node import TreeNode
+from typing import List, Optional
+
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        cols = {}
+        def worker(node: Optional[TreeNode], row: int, col: int) -> None:
+            if node:
+                value = node.val
+                if col not in cols:
+                    cols[col] = {}
+                rows = cols[col]
+                if not row in rows:
+                    rows[row] = [value]
+                else:
+                    rows[row].append(value)
+                worker(node.left, row + 1, col - 1)
+                worker(node.right, row + 1, col + 1)
+        worker(root, 0, 0)
+
+        results = []
+        for key in sorted(cols):
+            result = []
+            rows = cols[key]
+            for key in sorted(rows):
+                data = rows[key]
+                for value in sorted(data):
+                    result.append(value)
+            results.append(result)
+
+        return results
