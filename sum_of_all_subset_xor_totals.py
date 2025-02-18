f2e46f2ae8d1f25009203118b178a03bfdcf6dc6
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-17 18:24:35 -0800

    Added Sum of All Subset Xor Totals

---
+++ sum_of_all_subset_xor_totals.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def worker(index: int, current: int, n: int) -> int:
+            result = 0
+            if index == n:
+                result = current
+            else:
+                value = nums[index]
+                result += worker(index+1, current ^ value, n)
+                result += worker(index+1, current, n)
+            return result
+
+        n = len(nums)
+        result = worker(0, 0, n)
+
+        return result
---
+++ test_sum_of_all_subset_xor_totals.py
@@ -0,0 +1,13 @@
+from sum_of_all_subset_xor_totals import Solution
+
+def test_ex1():
+    nums = [1,3]
+    solution = Solution()
+    result = solution.subsetXORSum(nums)
+    assert result == 6
+
+def test_ex2():
+    nums = [5,1,6]
+    solution = Solution()
+    result = solution.subsetXORSum(nums)
+    assert result == 28
