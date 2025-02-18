925b99b8aa7aaa064f4cd535fbdb42540288ed58
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-17 17:30:11 -0800

    Added Construct Smallest Number from DI String

---
+++ construct_smallest_number_from_di_string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        n = len(pattern) + 1
+
+        used = [False for _ in range(n)]
+        current = [0 for _ in range(n)]
+
+        def worker(last: int, index: int) -> bool:
+            result = True
+            if index < n:
+                for i in range(n):
+                    if not used[i]:
+                        value = i + 1
+                        can_proceed = last == 0
+                        can_proceed = can_proceed or (pattern[index-1] == 'I' and value > last)
+                        can_proceed = can_proceed or (pattern[index-1] == 'D' and value < last)
+                        if can_proceed:
+                            current[index] = value
+                            used[i] = True
+                            result = worker(value, index+1)
+                            if result:
+                                return True
+                            else:
+                                current[index] = 0
+                                used[i] = False
+                        else:
+                            result = False
+
+            return result
+        worker(0, 0)
+
+        return "".join(map(str, current))
---
+++ test_construct_smallest_number_from_di_string.py
@@ -0,0 +1,13 @@
+from construct_smallest_number_from_di_string import Solution
+
+def test_ex1():
+    pattern = "IIIDIDDD"
+    solution = Solution()
+    result = solution.smallestNumber(pattern)
+    assert result == "123549876"
+
+def test_ex2():
+    pattern = "DDD"
+    solution = Solution()
+    result = solution.smallestNumber(pattern)
+    assert result == "4321"
