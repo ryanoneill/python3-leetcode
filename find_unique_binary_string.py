2b67f6f49cd21fd6b73a4d9cb566ccc2c825c604
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-19 17:49:55 -0800

    Added Find Unique Binary String

---
+++ find_unique_binary_string.py
@@ -0,0 +1,24 @@
+from typing import List, Set
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        def worker(items: List[str], index: int, n: int, seen: Set[str]) -> str:
+            if index == n:
+                candidate = "".join(items)
+                if candidate in seen:
+                    candidate = ""
+                return candidate
+            else:
+                for digit in ["0", "1"]:
+                    items.append(digit)
+                    result = worker(items, index+1, n, seen)
+                    if result != "":
+                        return result
+                    else:
+                        items.pop()
+            return ""
+
+        seen = set(nums)
+        n = len(nums[0])
+        result = worker([], 0, n, seen)
+        return result
---
+++ test_find_unique_binary_string.py
@@ -0,0 +1,19 @@
+from find_unique_binary_string import Solution
+
+def test_ex1():
+    nums = ["01", "10"]
+    solution = Solution()
+    result = solution.findDifferentBinaryString(nums)
+    assert result == "00"
+
+def test_ex2():
+    nums = ["00", "01"]
+    solution = Solution()
+    result = solution.findDifferentBinaryString(nums)
+    assert result == "10"
+
+def test_ex3():
+    nums = ["111","011","001"]
+    solution = Solution()
+    result = solution.findDifferentBinaryString(nums)
+    assert result == "000"
