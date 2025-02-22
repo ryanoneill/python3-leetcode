1392f26f421417c26e200ffeee19140d22c19226
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-21 16:21:12 -0800

    Added Binary Tree Level Order Traversal II

---
+++ binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,24 @@
+from queue import deque
+from tree_node import TreeNode
+from typing import Optional, List
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        results = []
+        items = deque()
+        if root:
+            items.append(root)
+
+        while items:
+            n = len(items)
+            level = []
+            for _ in range(n):
+                item = items.popleft()
+                level.append(item.val)
+                if item.left:
+                    items.append(item.left)
+                if item.right:
+                    items.append(item.right)
+            results.append(level)
+
+        return list(reversed(results))
---
+++ test_binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,18 @@
+from binary_tree_level_order_traversal_ii import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+   items = "[3,9,20,null,null,15,7]" 
+   codec = Codec()
+   root = codec.deserialize(items)
+   solution = Solution()
+   result = solution.levelOrderBottom(root)
+   assert result == [[15,7],[9,20],[3]]
+
+def test_ex2():
+    items = "[1]"
+    codec = Codec()
+    root = codec.deserialize(items)
+    solution = Solution()
+    result = solution.levelOrderBottom(root)
+    assert result == [[1]]
