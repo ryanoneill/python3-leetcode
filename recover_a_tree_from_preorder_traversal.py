881b28554234131c523c496e7b2aa2cfc8744591
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-21 16:41:12 -0800

    Added Recover a Tree from Preorder Traversal

---
+++ recover_a_tree_from_preorder_traversal.py
@@ -0,0 +1,31 @@
+from tree_node import TreeNode
+from typing import List, Optional
+
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        values = traversal.split("-")
+        result = TreeNode(int(values[0]))
+        nodes = [result]
+
+        i = 1
+        n = len(values)
+        while i < n:
+            level = 1
+            value = values[i]
+            while not value.isnumeric():
+                level += 1
+                i += 1
+                value = values[i]
+            while len(nodes) > level:
+                nodes.pop()
+            parent = nodes[-1]
+            node = TreeNode(int(value))
+            nodes.append(node)
+            if not parent.left:
+                parent.left = node
+            else:
+                parent.right = node
+            i += 1
+
+        return result
+
---
+++ test_recover_a_tree_from_preorder_traversal.py
@@ -0,0 +1,27 @@
+from recover_a_tree_from_preorder_traversal import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    traversal = "1-2--3--4-5--6--7"
+    solution = Solution()
+    result = solution.recoverFromPreorder(traversal)
+    codec = Codec()
+    values = codec.serialize(result)
+    assert values == "[1,2,5,3,4,6,7]"
+
+def test_ex2():
+    traversal = "1-2--3---4-5--6---7"
+    solution = Solution()
+    result = solution.recoverFromPreorder(traversal)
+    codec = Codec()
+    values = codec.serialize(result)
+    assert values == "[1,2,5,3,null,6,null,4,null,7]"
+
+def test_ex3():
+    traversal = "1-401--349---90--88"
+    solution = Solution()
+    result = solution.recoverFromPreorder(traversal)
+    codec = Codec()
+    values = codec.serialize(result)
+    assert values == "[1,401,null,349,88,90]"
+
