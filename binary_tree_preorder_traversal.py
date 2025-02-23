39ba1cf2b6b02777c50882ee24faea13080fce99
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-22 16:38:32 -0800

    Added Binary Tree Preorder Traversal

---
+++ binary_tree_preorder_traversal.py
@@ -0,0 +1,14 @@
+from tree_node import TreeNode
+from typing import List, Optional
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def worker(results: List[int], node: Optional[TreeNode]):
+            if node:
+                results.append(int(node.val))
+                worker(results, node.left)
+                worker(results, node.right)
+
+        results = []
+        worker(results, root)
+        return results
