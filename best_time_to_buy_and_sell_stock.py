fff7e8d45454f1783f1e9ce0f3486119630c7758
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-25 20:38:32 -0800

    Added Best Time to Buy and Sell Stock

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        cost = pow(2, 32) - 1
+
+        for price in prices:
+            if price < cost:
+                cost = price
+            profit = price - cost
+            result = max(result, profit)
+
+        return result
---
+++ test_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,13 @@
+from best_time_to_buy_and_sell_stock import Solution
+
+def test_ex1():
+    prices = [7,1,5,3,6,4]
+    solution = Solution()
+    result = solution.maxProfit(prices)
+    assert result == 5
+
+def test_ex2():
+    prices = [7,6,4,3,1]
+    solution = Solution()
+    result = solution.maxProfit(prices)
+    assert result == 0
