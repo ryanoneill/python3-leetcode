eb30bfd2d1c182ea7d021fd46fa796a6658632d4
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-25 17:31:53 -0800

    Added Maximum Absolute Sum of Any Subarray

---
+++ maximum_absolute_sum_of_any_subarray.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        posSum = 0
+        currentPosSum = 0
+
+        negSum = 0
+        currentNegSum = 0
+        for num in nums:
+            if currentPosSum < 0:
+                currentPosSum = num
+            else:
+                currentPosSum += num
+            posSum = max(posSum, currentPosSum)
+
+            if currentNegSum > 0:
+                currentNegSum = num
+            else:
+                currentNegSum += num
+            negSum = min(negSum, currentNegSum)
+
+        return max(posSum, abs(negSum))
