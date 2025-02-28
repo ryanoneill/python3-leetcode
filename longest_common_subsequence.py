2ee628dda4228fe239cb594edece015d6d416e6c
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-27 21:13:27 -0800

    Added Longest Common Subsequence

---
+++ longest_common_subsequence.py
@@ -0,0 +1,15 @@
+from typing import Dict
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        for j in reversed(range(n)):
+            for i in reversed(range(m)):
+                if text2[j] == text1[i]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
---
+++ test_longest_common_subsequence.py
@@ -0,0 +1,15 @@
+from longest_common_subsequence import Solution
+
+def test_ex1():
+    text1 = "abcde"
+    text2 = "ace"
+    solution = Solution()
+    result = solution.longestCommonSubsequence(text1, text2)
+    assert result == 3
+
+def test_ex2():
+    text1 = "abc"
+    text2 = "abc"
+    solution = Solution()
+    result = solution.longestCommonSubsequence(text1, text2)
+    assert result == 3
