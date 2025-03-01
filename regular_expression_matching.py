f507c57cd0ec8f2678ebd23856d0cdcdfa1cdb47
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-01 11:50:26 -0800

    Added Regular Expression Matching

---
+++ regular_expression_matching.py
@@ -0,0 +1,75 @@
+from abc import abstractmethod
+
+class Pattern:
+    def __init__(self, value: str):
+        self.value = value
+
+    def matches_at(self, s: str, index: int) -> bool:
+        result = False
+        if self.value == ".":
+            result = True
+        else:
+            result = s[index] == self.value
+        return result
+
+    @abstractmethod
+    def is_multiple(self) -> bool:
+        pass
+
+class Single(Pattern):
+    def is_multiple(self) -> bool:
+        return False
+
+class Multiple(Pattern):
+    def is_multiple(self) -> bool:
+        return True
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        patterns = []
+        for i in range(len(p)):
+            value = p[i]
+            if value == "*":
+                previous = patterns.pop()
+                multiple = Multiple(previous.value)
+                patterns.append(multiple)
+            else:
+                single = Single(value)
+                patterns.append(single)
+
+        cache = {}
+        def worker(position: int, index: int) -> bool:
+            nonlocal cache
+            nonlocal patterns
+            nonlocal s
+            if index == len(s):
+                if position == len(patterns):
+                    return True
+                else:
+                    pattern = patterns[position]
+                    if pattern.is_multiple():
+                        return worker(position+1, index)
+                    else:
+                        return False
+            elif position == len(patterns):
+                return False
+            elif (position, index) in cache:
+                return cache[(position, index)]
+            else:
+                pattern = patterns[position]
+                if pattern.is_multiple():
+                    result = pattern.matches_at(s, index)
+                    if result:
+                        result = worker(position, index+1)
+                        result = result or worker(position+1, index+1)
+                        result = result or worker(position+1, index)
+                    else:
+                        result = worker(position+1, index)
+                else:
+                    result = pattern.matches_at(s, index)
+                    if result:
+                        result = worker(position+1, index+1)
+                cache[(position, index)] = result
+                return result
+
+        return worker(0, 0)
---
+++ test_regular_expression_matching.py
@@ -0,0 +1,50 @@
+from regular_expression_matching import Solution
+
+def test_ex1():
+    s = "aa"
+    p = "a"
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert not result
+
+def test_ex2():
+    s = "aa"
+    p = "a*"
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert result
+
+def test_ex3():
+    s = "ab"
+    p = ".*"
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert result
+
+def test_ex4():
+    s = "aab"
+    p = "c*a*b"
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert result
+
+def test_ex5():
+    s = "mississippi"
+    p = "mis*is*p*."
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert not result
+
+def test_ex6():
+    s = "a"
+    p = "ab*"
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert result
+
+def test_ex7():
+    s = "bbbba"
+    p = ".*a*a"
+    solution = Solution()
+    result = solution.isMatch(s, p)
+    assert result
