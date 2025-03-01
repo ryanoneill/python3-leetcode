f0b113418502d819c482628528959e63b46e1725
Ryan O'Neill <ryanoneill@gmail.com>
2025-02-28 16:31:35 -0800

    Added Shortest Common Supersequence

---
+++ test_shortest_common_supersequence.py
@@ -0,0 +1,15 @@
+from shortest_common_supersequence import Solution
+
+def test_ex1():
+    str1 = "abac"
+    str2 = "cab"
+    solution = Solution()
+    result = solution.shortestCommonSupersequence(str1, str2)
+    assert result == "cabac"
+
+def test_ex2():
+    str1 = "aaaaaaaa"
+    str2 = "aaaaaaaa"
+    solution = Solution()
+    result = solution.shortestCommonSupersequence(str1, str2)
+    assert result == "aaaaaaaa"
