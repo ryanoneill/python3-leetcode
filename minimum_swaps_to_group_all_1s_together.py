dd544059915c1aae828a4f56f7ea20f8075bbdf4
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-01 11:06:12 -0800

    Added Minimum Swaps to Group All 1s Together

---
+++ minimum_swaps_to_group_all_1s_together.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def minSwaps(self, data: List[int]) -> int:
+        window = 0
+        for num in data:
+            if num == 1:
+                window += 1
+
+        
+        count = 0
+        for i in range(window):
+            if data[i] == 1:
+                count += 1
+
+        n = len(data)
+        result = window - count
+        for j in range(window, n):
+            if data[j - window] == 1:
+                count -= 1
+            if data[j] == 1:
+                count += 1
+            result = min(result, window - count)
+        
+        return result
