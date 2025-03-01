2395eb6a9f3401a7174ce3b750f0e8d9c01afa77
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-01 14:50:27 -0800

    Added Add Binary

---
+++ add_binary.py
@@ -0,0 +1,44 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_n = len(a)
+        b_n = len(b)
+
+        a_index = 0
+        b_index = 0
+
+        carry = False
+        result = []
+        for index in range(max(a_n, b_n)):
+            a_value = "0"
+            b_value = "0" 
+
+            if index < a_n:
+                a_value = a[a_n - index - 1]
+            if index < b_n:
+                b_value = b[b_n - index - 1]
+
+            sum = ""
+            if a_value == "1" and b_value == "1":
+                sum = "0"
+                if carry:
+                    sum = "1"
+                carry = True
+            elif a_value == "1" and b_value == "0":
+                sum = "1"
+                if carry:
+                    sum = "0"
+            elif a_value == "0" and b_value == "1":
+                sum = "1"
+                if carry:
+                    sum = "0"
+            else:
+                sum = "0"
+                if carry:
+                    sum = "1"
+                    carry = False
+
+            result.append(sum)
+        if carry:
+            result.append("1")
+
+        return "".join(reversed(result))
---
+++ test_add_binary.py
@@ -0,0 +1,15 @@
+from add_binary import Solution
+
+def test_ex1():
+    a = "11"
+    b = "1"
+    solution = Solution()
+    result = solution.addBinary(a, b)
+    assert result == "100"
+
+def test_ex2():
+    a = "1010"
+    b = "1011"
+    solution = Solution()
+    result = solution.addBinary(a, b)
+    assert result == "10101"
