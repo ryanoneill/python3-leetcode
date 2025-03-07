6f68a26e42b8fe37bfcc97153ba9c9021b515d93
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-06 18:41:37 -0800

    Added Find Missing and Repeated Values

---
+++ find_missing_and_repeated_values.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        seen = set()
+        actual = 0
+        dupe = 0
+        for row in grid:
+            for value in row:
+                if value in seen:
+                    dupe = value
+                else:
+                    seen.add(value)
+                    actual += value
+
+        n = len(grid) * len(grid)
+        expected = n * (n + 1) // 2
+        missing = expected - actual
+        return [dupe, missing]
