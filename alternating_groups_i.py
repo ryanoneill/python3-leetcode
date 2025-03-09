19ac5857a43614f11a6aa9db5f565b483e4379fc
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-08 17:24:02 -0800

    Added Alternating Colors I

---
+++ alternating_groups_i.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        n = len(colors)
+        result = 0
+        for i in range(n):
+            first = colors[i % n]
+            second = colors[(i+1) % n]
+            third = colors[(i+2) % n]
+            if first != second and second != third:
+                result += 1
+        return result
