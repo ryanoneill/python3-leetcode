f85dd2622c0fa2b2aee19f6cc02b660000932973
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-12 18:17:09 -0700

    Added Find the Distinct Difference Array

---
+++ find_the_distinct_difference_array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+
+        seen = set()
+        for i, num in enumerate(nums):
+            seen.add(num)
+            result[i] = len(seen)
+        
+        seen.clear()
+        for i in range(n-1, -1, -1):
+            num = nums[i]
+            seen.add(num)
+            if i > 0:
+                result[i-1] -= len(seen)
+
+        return result
---
+++ test_find_the_distinct_difference_array.py
@@ -0,0 +1,9 @@
+from find_the_distinct_difference_array import Solution
+
+def test_ex1():
+    nums = [1,2,3,4,5]
+    solution = Solution()
+    result = solution.distinctDifferenceArray(nums)
+    assert result == [-3,-1,1,3,5]
+
+
