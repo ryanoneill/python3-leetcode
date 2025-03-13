b122a5f3ec22a8a7a0a693b7715c7f2d44bee2d2
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-12 18:28:08 -0700

    Added Binary Search

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        result = -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            num = nums[mid]
+            if num == target:
+                result = mid
+                break
+            elif num < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
---
+++ test_binary_search.py
@@ -0,0 +1,15 @@
+from binary_search import Solution
+
+def test_ex1():
+    nums = [-1,0,3,5,9,12]
+    target = 9
+    solution = Solution()
+    result = solution.search(nums, target)
+    assert result == 4
+
+def test_ex2():
+    nums = [-1,0,3,5,9,12]
+    target = 2
+    solution = Solution()
+    result = solution.search(nums, target)
+    assert result == -1
