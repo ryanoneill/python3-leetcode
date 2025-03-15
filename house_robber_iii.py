5b0421118cc4bba049481a9abb25f612ee0345eb
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-14 18:14:32 -0700

    Added House Robber III

---
+++ house_robber_iii.py
@@ -0,0 +1,31 @@
+from tree_node import TreeNode
+
+from typing import Optional
+
+# TODO: Improve Runtime
+class Solution:
+    def worker(self, cache: dict, node: Optional[TreeNode], skip: bool, num: int) -> int:
+        key = (num, skip)
+        result = 0
+        if key in cache:
+            result = cache[key]
+        else:
+            if node:
+                result = (self.worker(cache, node.left, False, num * 2) +
+                         self.worker(cache, node.right, False, num * 2 + 1))
+                if not skip:
+                    candidate = (node.val
+                        + self.worker(cache, node.left, True, num * 2)
+                        + self.worker(cache, node.right, True, num * 2 + 1))
+                    result = max(result, candidate)
+            cache[key] = result
+
+        return result
+
+    def rob(self, root: Optional[TreeNode]) -> int:
+        cache = {}
+        yes = self.worker(cache, root, True, 1)
+        no = self.worker(cache, root, False, 1)
+        result = max(yes, no)
+
+        return result 
---
+++ test_house_robber_iii.py
@@ -0,0 +1,18 @@
+from house_robber_iii import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    data = "[3,2,3,null,3,null,1]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    result = solution.rob(root)
+    assert result == 7
+
+def test_ex2():
+    data = "[3,4,5,1,3,null,1]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    result = solution.rob(root)
+    assert result == 9
