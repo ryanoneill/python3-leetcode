4907d5e08f0d2daf972e7457edb67a60e9054f36
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-16 17:07:24 -0700

    Added Divide Array into Equal Parts

---
+++ divide_array_into_equal_pairs.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+
+        result = len(seen) == 0
+        return result
+
---
+++ test_divide_array_into_equal_pairs.py
@@ -0,0 +1,13 @@
+from divide_array_into_equal_pairs import Solution
+
+def test_ex1():
+    nums = [3,2,3,2,2,2]
+    solution = Solution()
+    result = solution.divideArray(nums)
+    assert result
+
+def test_ex2():
+    nums = [1,2,3,4]
+    solution = Solution()
+    result = solution.divideArray(nums)
+    assert not result
