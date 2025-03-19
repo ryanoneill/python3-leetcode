9ff9f7a6bcb4e6d6f23ea382ab4d1442bd2a8d6a
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-18 17:18:51 -0700

    Added Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ minimum_operations_to_make_binary_array_elements_equal_to_one_i.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        result = -1
+        if n == 1:
+            if nums == [1]:
+                result = 0
+        elif n == 2:
+            if nums == [1, 1]:
+                result = 0
+        else:
+            result = 0
+            for i in range(n-2):
+                if nums[i] == 0:
+                    result += 1
+                    nums[i] = 1
+                    nums[i+1] ^= 1
+                    nums[i+2] ^= 1
+            if nums[-2] != 1 or nums[-1] != 1:
+                result = -1
+
+        return result
---
+++ test_minimum_operations_to_make_binary_array_elements_equal_to_one_i.py
@@ -0,0 +1,19 @@
+from minimum_operations_to_make_binary_array_elements_equal_to_one_i import Solution
+
+def test_ex1():
+    nums = [0,1,1,1,0,0]
+    solution = Solution()
+    result = solution.minOperations(nums)
+    assert result == 3
+
+def test_ex2():
+    nums = [0,1,1,1]
+    solution = Solution()
+    result = solution.minOperations(nums)
+    assert result == -1
+
+def test_ex3():
+    nums = [1,0,0,1,1,0,1,1,1,0,0,0,1,0,1]
+    solution = Solution()
+    result = solution.minOperations(nums)
+    assert result == 9
