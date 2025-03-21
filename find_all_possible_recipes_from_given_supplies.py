cae999709afbc754fa014c3acc05bde3b4357371
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-21 14:55:03 -0700

    Added Find All Recipes from Given Supplies

---
+++ find_all_possible_recipes_from_given_supplies.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, deque
+from typing import List
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        haves = set(supplies)
+        indices = {recipe: index for index, recipe in enumerate(recipes)}
+        dependencies = defaultdict(list)
+        indegrees = [0] * len(recipes)
+
+        for index, needs in enumerate(ingredients):
+            for item in needs:
+                if item not in haves:
+                    dependencies[item].append(recipes[index])
+                    indegrees[index] += 1
+
+        results = []
+        queue = deque(index for index, count in enumerate(indegrees) if count == 0)
+        while queue:
+            index = queue.popleft()
+            recipe = recipes[index]
+            results.append(recipe)
+
+            for dependency in dependencies[recipe]:
+                dep_index = indices[dependency]
+                indegrees[dep_index] -= 1
+                if indegrees[dep_index] == 0:
+                    queue.append(dep_index)
+
+        return results
---
+++ test_find_all_possible_recipes_from_given_supplies.py
@@ -0,0 +1,49 @@
+from find_all_possible_recipes_from_given_supplies import Solution
+
+def test_ex1():
+    recipes = ["bread"]
+    ingredients = [["yeast", "flour"]]
+    supplies = ["yeast", "flour", "corn"]
+    solution = Solution()
+    results = solution.findAllRecipes(recipes, ingredients, supplies)
+    results.sort()
+    assert results == ["bread"]
+
+def test_ex2():
+    recipes = ["bread","sandwich"]
+    ingredients = [["yeast","flour"],["bread","meat"]]
+    supplies = ["yeast","flour","meat"]
+    solution = Solution()
+    results = solution.findAllRecipes(recipes, ingredients, supplies)
+    results.sort()
+    assert results == ["bread", "sandwich"]
+
+def test_ex3():
+    recipes = ["bread","sandwich","burger"]
+    ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]]
+    supplies = ["yeast","flour","meat"]
+    solution = Solution()
+    results = solution.findAllRecipes(recipes, ingredients, supplies)
+    results.sort()
+    assert results == ["bread", "burger", "sandwich"]
+
+def test_ex4():
+    recipes = ["xevvq","izcad","p","we","bxgnm","vpio","i","hjvu","igi","anp","tokfq","z","kwdmb","g","qb","q","b","hthy"] 
+    ingredients = [["wbjr"],["otr","fzr","g"],["fzr","wi","otr","xgp","wbjr","igi","b"],["fzr","xgp","wi","otr","tokfq","izcad","igi","xevvq","i","anp"],["wi","xgp","wbjr"],["wbjr","bxgnm","i","b","hjvu","izcad","igi","z","g"],["xgp","otr","wbjr"],["wbjr","otr"],["wbjr","otr","fzr","wi","xgp","hjvu","tokfq","z","kwdmb"],["xgp","wi","wbjr","bxgnm","izcad","p","xevvq"],["bxgnm"],["wi","fzr","otr","wbjr"],["wbjr","wi","fzr","xgp","otr","g","b","p"],["otr","fzr","xgp","wbjr"],["xgp","wbjr","q","vpio","tokfq","we"],["wbjr","wi","xgp","we"],["wbjr"],["wi"]]
+    supplies = ["wi","otr","wbjr","fzr","xgp"]
+    solution = Solution()
+    results = solution.findAllRecipes(recipes, ingredients, supplies)
+    results.sort()
+    expected = ["xevvq","izcad","bxgnm","i","hjvu","tokfq","z","g","b","hthy"]
+    expected.sort()
+    assert results == expected
+
+def test_ex5():
+    recipes = ["burger", "bread", "sandwich"]
+    ingredients = [["sandwich","meat","bread"],["yeast","flour"],["bread","meat"]]
+    supplies = ["yeast","flour","meat"]
+    solution = Solution()
+    results = solution.findAllRecipes(recipes, ingredients, supplies)
+    results.sort()
+    assert results == ["bread", "burger", "sandwich"]
+
