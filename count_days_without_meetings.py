82c292755216caa197c2df545a4eef644e40b593
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-23 20:24:44 -0700

    Added Count Days Without Meetings

---
+++ count_days_without_meetings.py
@@ -0,0 +1,23 @@
+from typing import List
+from heapq import heappop, heapify
+
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        n = days
+        result = 0
+        current = 1
+        heapify(meetings)
+        while meetings:
+            start, end = heappop(meetings)
+            if start == current:
+                current = end + 1
+            elif start > current:
+                result += start - current
+                current = end + 1
+            elif end >= current:
+                current = end + 1
+
+        if current <= n:
+            result += n - current + 1
+
+        return result
