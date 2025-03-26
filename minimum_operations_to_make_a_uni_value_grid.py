cd2ad63b6ceb1d8bb02b56e817aad755d202c437
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-26 08:43:40 -0700

    Added Minimum Operations to Make a Uni-Value Grid

---
+++ minimum_operations_to_make_a_uni_value_grid.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        values = []
+        for row in grid:
+            for num in row:
+                values.append(num)
+
+        values.sort()
+        n = len(values)
+        median = values[n // 2]
+        moddian = median % x
+
+        result = 0
+        for value in values:
+            if value % x != moddian:
+                result = -1
+                break
+            else:
+                result += abs(value - median) // x
+
+        return result
+
