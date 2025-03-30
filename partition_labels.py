65c0e312feee55f4018768a375ae0cb42e07a200
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-30 12:09:03 -0700

    Added Partition Labels

---
+++ partition_labels.py
@@ -0,0 +1,33 @@
+from typing import List
+from heapq import heappush, heappop
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        first = {}
+        last = {}
+
+        for index, letter in enumerate(s):
+            if letter not in first:
+                first[letter] = index
+            last[letter] = index
+
+        results = []
+        heap = []
+        for key in first.keys():
+            i = first[key]
+            j = last[key]
+            heappush(heap, (i, j))
+
+        start = 0
+        end = 0
+        while heap:
+            i, j = heappop(heap)
+            if i > end:
+                results.append(end - start + 1)
+                start = i
+                end = j
+            elif j > end:
+                end = j
+                
+        results.append(end - start + 1)
+        return results
---
+++ test_partition_labels.py
@@ -0,0 +1,13 @@
+from partition_labels import Solution
+
+def test_ex1():
+    s = "ababcbacadefegdehijhklij"
+    solution = Solution()
+    result = solution.partitionLabels(s)
+    assert result == [9,7,8]
+
+def test_ex2():
+    s = "eccbbbbdec"
+    solution = Solution()
+    result = solution.partitionLabels(s)
+    assert result == [10]
