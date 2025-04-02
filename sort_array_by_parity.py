e49d071f41482cfa11269baf059d2286b3fbef60
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-01 19:58:19 -0700

    Added Sort Array by Parity

---
+++ sort_array_by_parity.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left = 0
+        right = n-1
+        result = [0] * n
+
+        for num in nums:
+            if num % 2 == 0:
+                result[left] = num
+                left += 1
+            else:
+                result[right] = num
+                right -= 1
+
+        return result
+
+
---
+++ test_sort_array_by_parity.py
@@ -0,0 +1,13 @@
+from sort_array_by_parity import Solution
+
+def test_ex1():
+    nums = [3,1,2,4]
+    solution = Solution()
+    result = solution.sortArrayByParity(nums)
+    assert result == [2,4,1,3]
+
+def test_ex2():
+    nums = [0]
+    solution = Solution()
+    result = solution.sortArrayByParity(nums)
+    assert result == [0]
