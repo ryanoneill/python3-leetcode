44154f4a9a237a4bb08cda0b126b92d88fd8eae7
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-02 14:07:46 -0700

    Added Triangle

---
+++ triangle.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[0] * (i + 1) for i in range(n)]
+
+        for i, row in enumerate(reversed(triangle)):
+            index = n - 1 - i
+            for j, value in enumerate(row):
+                if i == 0:
+                    dp[index][j] = value
+                else:
+                    dp[index][j] = value + min(dp[index+1][j], dp[index+1][j+1])
+
+        return dp[0][0]
