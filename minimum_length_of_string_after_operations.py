b994ee52e849e2e4ac04d1683770c6174d9f5707
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-02 18:21:13 -0700

    Added Minimum Length of String After Operations

---
+++ minimum_length_of_string_after_operations.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        result = 0
+        counter = Counter(s)
+        for value in counter.values():
+            if value % 2 == 1:
+                result += 1
+            else:
+                result += 2
+
+        return result
---
+++ test_minimum_length_of_string_after_operations.py
@@ -0,0 +1,13 @@
+from minimum_length_of_string_after_operations import Solution
+
+def test_ex1():
+    s = "abaacbcbb"
+    solution = Solution()
+    result = solution.minimumLength(s)
+    assert result == 5
+
+def test_ex2():
+    s = "aa"
+    solution = Solution()
+    result = solution.minimumLength(s)
+    assert result == 2
