7d3a2903311a474025c04e91a6c6e76986e00d55
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-03 11:12:48 -0700

    Added Restore IP Address

---
+++ restore_ip_address.py
@@ -0,0 +1,43 @@
+from typing import List
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        results = []
+        n = len(s)
+
+        def is_ok(previous: int, current: int) -> bool:
+            result = True
+            sub = s[previous:current]
+
+            if sub[0] == '0' and len(sub) > 1:
+                result = False
+            elif int(sub) > 255:
+                result = False
+
+            return result
+
+        def backtrack(places: List[int], start: int) -> None:
+            if len(places) == 3:
+                first = s[0:places[0]]
+                second = s[places[0]:places[1]]
+                third = s[places[1]:places[2]]
+                fourth = s[places[2]:]
+                results.append(first + "." + second + "." + third + "." + fourth)
+            else:
+                for i in range(start, n):
+                    previous = 0
+                    if len(places) > 0:
+                        previous = places[-1]
+                    proceed = is_ok(previous, i)
+                    if len(places) == 2:
+                        proceed = proceed and is_ok(i, n)
+
+                    if proceed:
+                        places.append(i)
+                        backtrack(places, i+1)
+                        places.pop()
+
+        backtrack([], 1)
+
+        return results
+
---
+++ test_restore_ip_address.py
@@ -0,0 +1,19 @@
+from restore_ip_address import Solution
+
+def test_ex1():
+    s = "25525511135"
+    solution = Solution()
+    results = solution.restoreIpAddresses(s)
+    assert results == ["255.255.11.135", "255.255.111.35"]
+
+def test_ex2():
+    s = "0000"
+    solution = Solution()
+    results = solution.restoreIpAddresses(s)
+    assert results == ["0.0.0.0"]
+
+def test_ex3():
+    s = "101023"
+    solution = Solution()
+    results = solution.restoreIpAddresses(s)
+    assert results == ["1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3"]
