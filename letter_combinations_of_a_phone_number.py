53d845b5a6ba6a54ad5708c3482c97cea3b27bb1
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-03 07:14:47 -0700

    Added Letter Combinations of a Phone Number

---
+++ letter_combinations_of_a_phone_number.py
@@ -0,0 +1,36 @@
+from typing import Set, List
+
+class Solution:
+    def digit_to_letters(self, digit: str) -> Set[str]:
+        if digit == "2":
+            return {"a", "b", "c"}
+        elif digit == "3":
+            return {"d", "e", "f"}
+        elif digit == "4":
+            return {"g", "h", "i"}
+        elif digit == "5":
+            return {"j", "k", "l"}
+        elif digit == "6":
+            return {"m", "n", "o"}
+        elif digit == "7":
+            return {"p", "q", "r", "s"}
+        elif digit == "8":
+            return {"t", "u", "v"}
+        elif digit == "9":
+            return {"w", "x", "y", "z"}
+        return set()
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        results = []
+        for digit in digits:
+            letters = self.digit_to_letters(digit)
+            if results == []:
+                results = list(letters)
+            else:
+                original = results
+                results = []
+                for letter in letters:
+                    for orig in original:
+                        results.append(orig + letter)
+
+        return results
---
+++ test_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,22 @@
+from letter_combinations_of_a_phone_number import Solution
+
+def test_ex1():
+    digits = "23"
+    solution = Solution()
+    result = solution.letterCombinations(digits)
+    result.sort()
+    assert result == ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+def test_ex2():
+    digits = ""
+    solution = Solution()
+    result = solution.letterCombinations(digits)
+    result.sort()
+    assert result == []
+
+def test_ex3():
+    digits = "2"
+    solution = Solution()
+    result = solution.letterCombinations(digits)
+    result.sort()
+    assert result == ["a","b","c"]
