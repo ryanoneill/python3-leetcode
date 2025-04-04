bb923f0b038d801ef09e34a4c55b1604f2a58c34
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-03 18:50:38 -0700

    Added Sum Root to Leaf Numbers

---
+++ sum_root_to_leaf_numbers.py
@@ -0,0 +1,25 @@
+from tree_node import TreeNode
+from typing import Optional
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(path: str, node: TreeNode) -> int:
+            combined = path + str(node.val)
+            if node.left and node.right:
+                left = dfs(combined, node.left)
+                right = dfs(combined, node.right)
+                result = left + right
+            elif node.left:
+                result = dfs(combined, node.left)
+            elif node.right:
+                result = dfs(combined, node.right)
+            else:
+                result = int(combined)
+
+            return result
+
+        result = 0
+        if root:
+            result = dfs("", root)
+        return result
+
---
+++ test_sum_root_to_leaf_numbers.py
@@ -0,0 +1,18 @@
+from sum_root_to_leaf_numbers import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    items = "[1,2,3]"
+    codec = Codec()
+    root = codec.deserialize(items)
+    solution = Solution()
+    result = solution.sumNumbers(root)
+    assert result == 25
+
+def test_ex2():
+    items = "[4,9,0,5,1]"
+    codec = Codec()
+    root = codec.deserialize(items)
+    solution = Solution()
+    result = solution.sumNumbers(root)
+    assert result == 1026
