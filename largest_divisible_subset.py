9393fd4ec5c082ccd94655bc773a52bdf22ef131
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-05 18:02:18 -0700

    Added Largest Divisible Subset

---
+++ largest_divisible_subset.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution(object):
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        subsets = {}
+
+        for num in nums:
+            result = {}
+            for key in subsets.keys():
+                if num % key == 0: 
+                    value = subsets[key]
+                    if len(value) > len(result):
+                        result = value
+
+            result = set(result)
+            result.add(num)
+            subsets[num] = result
+
+        return sorted(list(max(subsets.values(), key=len)))
