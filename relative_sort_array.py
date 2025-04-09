53732bebfb184bed5ea8a033ac898d90a9f1a795
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-08 22:49:40 -0700

    Added Relative Sort Array

---
+++ relative_sort_array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        rels = {num: i for i, num in enumerate(arr2)}
+
+        def sortKey(x: int) -> int:
+            if x in rels:
+                return rels[x]
+            else:
+                return 2000 + x
+
+        results = sorted(arr1, key=sortKey)
+        return results
+
