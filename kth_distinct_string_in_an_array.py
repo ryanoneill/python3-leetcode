6501900e47e4f79e63ed773c9011e5383bc6d033
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-08 21:48:58 -0700

    Added Kth Distinct String in an Array

---
+++ kth_distinct_string_in_an_array.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        result = ""
+        counts = Counter(arr)
+
+        j = 0
+        for value in arr:
+            if counts[value] == 1:
+                j += 1
+            if j == k:
+                result = value
+                break
+
+        return result
---
+++ test_kth_distinct_string_in_an_array.py
@@ -0,0 +1,22 @@
+from kth_distinct_string_in_an_array import Solution
+
+def test_ex1():
+    arr = ["d","b","c","b","c","a"]
+    k = 2
+    solution = Solution()
+    result = solution.kthDistinct(arr, k)
+    assert result == "a"
+
+def test_ex2():
+    arr = ["aaa","aa","a"]
+    k = 1
+    solution = Solution()
+    result = solution.kthDistinct(arr, k)
+    assert result == "aaa"
+
+def test_ex3():
+    arr = ["a","b","a"]
+    k = 3
+    solution = Solution()
+    result = solution.kthDistinct(arr, k)
+    assert result == ""
