8a5c5946232de6c208209bb6652ac3ed468680cf
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-08 23:31:51 -0700

    Added Check if Two String Arrays are Equivalent

---
+++ check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,21 @@
+from itertools import zip_longest
+from typing import Iterator, List
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        def gen(words: List[str]) -> Iterator[str]:
+            for word in words:
+                for letter in word:
+                    yield letter
+
+        word1_gen = gen(word1)
+        word2_gen = gen(word2)
+
+        result = True
+        for l1, l2 in zip_longest(word1_gen, word2_gen):
+            if l1 is None or l2 is None or l1 != l2:
+                result = False
+                break
+
+        return result
+
---
+++ test_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,29 @@
+from check_if_two_string_arrays_are_equivalent import Solution
+
+def test_ex1():
+    word1 = ["ab", "c"]
+    word2 = ["a", "bc"]
+    solution = Solution()
+    result = solution.arrayStringsAreEqual(word1, word2)
+    assert result
+
+def test_ex2():
+    word1 = ["a", "cb"]
+    word2 = ["ab", "c"]
+    solution = Solution()
+    result = solution.arrayStringsAreEqual(word1, word2)
+    assert not result
+
+def test_ex3():
+    word1  = ["abc", "d", "defg"]
+    word2 = ["abcddefg"]
+    solution = Solution()
+    result = solution.arrayStringsAreEqual(word1, word2)
+    assert result
+
+def test_ex4():
+    word1 = ["a"]
+    word2 = ["a", "b"]
+    solution = Solution()
+    result = solution.arrayStringsAreEqual(word1, word2)
+    assert not result
