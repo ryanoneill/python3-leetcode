04a426f95208880e0c2085251950a3961e60a7a1
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-09 20:10:29 -0700

    Added Count Integers with Even Digit Sum

---
+++ test_count_integers_with_even_digit_sum.py
@@ -0,0 +1,13 @@
+from count_integers_with_even_digit_sum import Solution
+
+def test_ex1():
+    num = 4
+    solution = Solution()
+    result = solution.countEven(num)
+    assert result == 2
+
+def test_ex2():
+    num = 30
+    solution = Solution()
+    result = solution.countEven(num)
+    assert result == 14
