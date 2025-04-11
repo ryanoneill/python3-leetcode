3898b161f671d237cf9d7d8ae54339aae11287f0
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-11 10:16:02 -0700

    Added Maximum Product of Two Elements in an Array

---
+++ maximum_product_of_two_elements_in_an_array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max1 = -1
+        max2 = -1
+
+        for num in nums:
+            if num > max1:
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max2 = num
+
+        return (max1 - 1) * (max2 - 1)
---
+++ test_maximum_product_of_two_elements_in_an_array.py
@@ -0,0 +1,19 @@
+from maximum_product_of_two_elements_in_an_array import Solution
+
+def test_ex1():
+    nums = [3,4,5,2]
+    solution = Solution()
+    result = solution.maxProduct(nums)
+    assert result == 12
+
+def test_ex2():
+    nums = [1,5,4,5]
+    solution = Solution()
+    result = solution.maxProduct(nums)
+    assert result == 16
+
+def test_ex3():
+    nums = [3,7]
+    solution = Solution()
+    result = solution.maxProduct(nums)
+    assert result == 12
