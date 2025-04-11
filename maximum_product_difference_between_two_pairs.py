6bd8b8f352811acbad211a343d667d9a748dcfb9
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-10 22:41:02 -0700

    Added Maximum Product Difference Between Two Pairs

---
+++ maximum_product_difference_between_two_pairs.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        max1 = -1
+        max2 = -1
+
+        min1 = 100000
+        min2 = 100000
+
+        for num in nums:
+            if num > max1:
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max2 = num
+
+            if num < min1:
+                min2 = min1
+                min1 = num
+            elif num < min2:
+                min2 = num
+
+        result = (max1 * max2) - (min1 * min2)
+        return result
---
+++ test_maximum_product_difference_between_two_pairs.py
@@ -0,0 +1,13 @@
+from maximum_product_difference_between_two_pairs import Solution
+
+def test_ex1():
+    nums = [5,6,2,7,4]
+    solution = Solution()
+    result = solution.maxProductDifference(nums)
+    assert result == 34
+
+def test_ex2():
+    nums = [4,2,5,9,7,4,8]
+    solution = Solution()
+    result = solution.maxProductDifference(nums)
+    assert result == 64
