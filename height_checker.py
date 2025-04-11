dd8ba83b8482906c47123da95735e9ad0b0629ee
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-10 22:14:54 -0700

    Added Height Checker

---
+++ height_checker.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        result = 0
+        counts = [0 for _ in range(101)]
+        for height in heights:
+            counts[height] += 1
+
+        expected = []
+        for i, count in enumerate(counts):
+            if count > 0:
+                for _ in range(count):
+                    expected.append(i)
+
+        for actual, expect in zip(heights, expected):
+            if actual != expect:
+                result += 1
+
+        return result
