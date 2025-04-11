89b34f0ff41ec531ac566aa58bd6c5c163fc3629
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-10 22:31:27 -0700

    Added Path Crossing

---
+++ path_crossing.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        result = False
+        seen = set()
+        pos = (0, 0)
+        seen.add(pos)
+
+        for direction in path:
+            x, y = pos
+            match direction:
+                case "N":
+                    pos = (x, y-1)
+                case "S":
+                    pos = (x, y+1)
+                case "E":
+                    pos = (x+1, y)
+                case "W":
+                    pos = (x-1, y)
+            if pos in seen:
+                result = True
+                break
+            else:
+                seen.add(pos)
+
+        return result
