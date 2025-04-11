f93b8f56c2762884da8b1ab17d9c218a52d47e12
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-11 16:16:59 -0700

    Added Find Words that can be Formed by Characters

---
+++ find_words_that_can_be_formed_by_characters.py
@@ -0,0 +1,23 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        result = 0
+        counts = Counter(chars)
+        for word in words:
+            n = len(word)
+            current = Counter(word)
+            word_result = True
+
+            for key, value in current.items():
+                if key not in counts:
+                    word_result = False
+                    break
+                elif counts[key] < value:
+                    word_result = False
+                    break
+            if word_result:
+                result += n
+
+        return result
---
+++ test_find_words_that_can_be_formed_by_characters.py
@@ -0,0 +1,15 @@
+from find_words_that_can_be_formed_by_characters import Solution
+
+def test_ex1():
+    words = ["cat", "bt", "hat", "tree"]
+    chars = "atach"
+    solution = Solution()
+    result = solution.countCharacters(words, chars)
+    assert result == 6
+
+def test_ex2():
+    words = ["hello", "world", "leetcode"]
+    chars = "welldonehoneyr"
+    solution = Solution()
+    result = solution.countCharacters(words, chars)
+    assert result == 10
