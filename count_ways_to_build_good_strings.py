e3e912ff34cff5e1b4b7652953b72927955f0615
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 12:28:25 -0700

    Added Count Ways to Build Good Strings

---
+++ count_ways_to_build_good_strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        MOD = 10**9 + 7
+
+        dp = [0] * (high + 1)
+        dp[0] = 1
+
+        for i in range(1, high+1):
+            value = 0
+            if i >= zero:
+                value += dp[i - zero]
+            if i >= one:
+                value += dp[i - one]
+            dp[i] = value % MOD
+
+        result = 0
+        for i in range(low, high+1):
+            result += dp[i]
+            result = result % MOD
+
+        return result
---
+++ test_count_ways_to_build_good_strings.py
@@ -0,0 +1,19 @@
+from count_ways_to_build_good_strings import Solution
+
+def test_ex1():
+    low = 3
+    high = 3
+    zero = 1
+    one = 1
+    solution = Solution()
+    result = solution.countGoodStrings(low, high, zero, one)
+    assert result == 8
+
+def test_ex2():
+    low = 2
+    high = 3
+    zero = 1
+    one = 2
+    solution = Solution()
+    result = solution.countGoodStrings(low, high, zero, one)
+    assert result == 5
