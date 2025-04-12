07a6b0d5e081b104bf07afb21bf8aacd89a5325f
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 13:46:52 -0700

    Added Uncommon Words from Two Sentences

---
+++ test_uncommon_words_from_two_sentences.py
@@ -0,0 +1,15 @@
+from uncommon_words_from_two_sentences import Solution
+
+def test_ex1():
+    s1 = "this apple is sweet"
+    s2 = "this apple is sour"
+    solution = Solution()
+    result = solution.uncommonFromSentences(s1, s2)
+    assert result == ["sweet", "sour"]
+
+def test_ex2():
+    s1 = "apple apple"
+    s2 = "banana"
+    solution = Solution()
+    result = solution.uncommonFromSentences(s1, s2)
+    assert result == ["banana"]
---
+++ uncommon_words_from_two_sentences.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        result = []
+        freq1 = Counter(s1.split(" "))
+        freq2 = Counter(s2.split(" "))
+
+        for word, value in freq1.items():
+            if value == 1 and word not in freq2:
+                result.append(word)
+
+        for word, value in freq2.items():
+            if value == 1 and word not in freq1:
+                result.append(word)
+
+        return result
