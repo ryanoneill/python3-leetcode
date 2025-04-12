ab2d9a206d46c5655cd05833c494b09919c910bf
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 13:14:20 -0700

    Added Number of Students Unable to Eat Lunch

---
+++ number_of_students_unable_to_eat_lunch.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        n = len(students)
+        squares = sum(students)
+        circles = n - squares
+
+        eaten = 0
+        for sandwich in sandwiches:
+            if sandwich == 1:
+                if squares > 0:
+                    squares -= 1
+                    eaten += 1
+                else:
+                    break
+            else:
+                if circles > 0:
+                    circles -= 1
+                    eaten += 1
+                else:
+                    break
+
+        result = n - eaten
+        return result
