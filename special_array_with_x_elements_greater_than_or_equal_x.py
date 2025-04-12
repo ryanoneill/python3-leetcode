b8839aed37c7f5494037ba18090dc5a355a16fdc
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 13:35:16 -0700

    Added Special Array with X Elements Greater Than or Equal to X

---
+++ special_array_with_x_elements_greater_than_or_equal_x.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        above = 0
+        counts = [0] * (101)
+        for num in nums:
+            if num <= 100:
+                counts[num] += 1
+            else:
+                above += 1
+
+        result = -1
+        for i in range(100, 0, -1):
+            above += counts[i]
+            if i == above:
+                result = i
+                break
+            elif i < above:
+                break
+
+        return result
---
+++ test_special_array_with_x_elements_greater_than_or_equal_x.py
@@ -0,0 +1,25 @@
+from special_array_with_x_elements_greater_than_or_equal_x import Solution
+
+def test_ex1():
+    nums = [3,5]
+    solution = Solution()
+    result = solution.specialArray(nums)
+    assert result == 2
+
+def test_ex2():
+    nums = [0,0]
+    solution = Solution()
+    result = solution.specialArray(nums)
+    assert result == -1
+
+def test_ex3():
+    nums = [0,4,3,0,4]
+    solution = Solution()
+    result = solution.specialArray(nums)
+    assert result == 3
+
+def test_ex4():
+    nums = [1000]
+    solution = Solution()
+    result = solution.specialArray(nums)
+    assert result == 1
