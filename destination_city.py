9c5606611db90591cd8a56f383eee1771ae88c32
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 12:40:18 -0700

    Added Destination City

---
+++ destination_city.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        outgoing = set()
+        overall = set()
+
+        for path in paths:
+            city_from, city_to = path
+
+            outgoing.add(city_from)
+
+            overall.add(city_from)
+            overall.add(city_to)
+
+        diff = overall.difference(outgoing)
+        return diff.pop()
