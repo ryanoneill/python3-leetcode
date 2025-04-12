048c00dd4c70fe74a3fb02ed221357b26b4e7c51
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 12:58:30 -0700

    Added Redistribute Characters to Make All Strings Equal

---
+++ redistribute_characters_to_make_all_strings_equal.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        counts = defaultdict(int)
+        for word in words:
+            for letter in word:
+                counts[letter] += 1
+
+        n = len(words)
+        result = True
+        for count in counts.values():
+            if count % n != 0:
+                result = False
+                break
+
+        return result
---
+++ test_redistribute_characters_to_make_all_strings_equal.py
@@ -0,0 +1,13 @@
+from redistribute_characters_to_make_all_strings_equal import Solution
+
+def test_ex1():
+    words = ["abc", "aabc", "bc"]
+    solution = Solution()
+    result = solution.makeEqual(words)
+    assert result
+
+def test_ex2():
+    words = ["ab", "a"]
+    solution = Solution()
+    result = solution.makeEqual(words)
+    assert not result
