3e27df7e9b247675019c25c0b6c50c003b6f86c0
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-13 11:41:15 -0700

    Added N-Ary Tree Preorder Traversal

---
+++ n_ary_tree_preorder_traversal.py
@@ -0,0 +1,17 @@
+from node import Node
+from typing import List, Optional
+
+class Solution:
+    def preorder(self, root: Optional['Node']) -> List[int]:
+        results = []
+
+        def worker(node: Optional['Node']):
+            if node:
+                results.append(node.val)
+                for child in node.children:
+                    worker(child)
+
+        worker(root)
+
+        return results
+
