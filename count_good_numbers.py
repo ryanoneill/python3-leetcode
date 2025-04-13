871868dbd794521486dfc2e6dc1cdd87beb17968
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-12 20:20:37 -0700

    Added Count Good Numbers

---
+++ count_good_numbers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10**9 + 7
+
+        results = [0] * 50
+        results[0] = 5
+        results[1] = 20
+        for i in range(2, 50):
+            result = results[i-1]
+            result *= result
+            result = result % MOD
+            results[i] = result
+
+        result = 0
+        for i in reversed(range(50)):
+            value = 2 ** i
+            while n >= value:
+                if result == 0:
+                    result = results[i]
+                else:
+                    result *= results[i]
+                    result = result % MOD
+                n -= value
+
+        return result
---
+++ test_count_good_numbers.py
@@ -0,0 +1,31 @@
+from count_good_numbers import Solution
+
+def test_ex1():
+    n = 1
+    solution = Solution()
+    result = solution.countGoodNumbers(n)
+    assert result == 5
+
+def test_ex2():
+    n = 4
+    solution = Solution()
+    result = solution.countGoodNumbers(n)
+    assert result == 400
+
+def test_ex3():
+    n = 50
+    solution = Solution()
+    result = solution.countGoodNumbers(n)
+    assert result == 564908303
+
+def test_ex4():
+    n = 10 ** 15
+    solution = Solution()
+    result = solution.countGoodNumbers(n)
+    assert result == 711414395
+
+def test_ex5():
+    n = 806166225460393
+    solution = Solution()
+    result = solution.countGoodNumbers(n)
+    assert result == 643535977
