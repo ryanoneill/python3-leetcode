1d740466affa58aea30bcd5c851cffd6f7ad58bd
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-13 11:36:11 -0700

    Added N-Ary Tree Postorder Traversal

---
+++ n_ary_tree_postorder_traversal.py
@@ -0,0 +1,19 @@
+from node import Node
+from typing import List, Optional
+
+class Solution:
+    def postorder(self, root: Optional['Node']) -> List[int]:
+        results = []
+        
+        def worker(node: Optional['Node']):
+            if node:
+                if node.children:
+                    for child in node.children:
+                        worker(child)
+
+                results.append(node.val)
+
+        worker(root)
+
+        return results
+        
