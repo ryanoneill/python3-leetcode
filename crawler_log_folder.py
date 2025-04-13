00cccd6ba42f308ac51f7240c898e4b4ec9f1fc9
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-13 10:02:29 -0700

    Added Crawler Log Folder

---
+++ crawler_log_folder.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for log in logs:
+            match log:
+                case "./":
+                    pass
+                case "../":
+                    if stack: stack.pop()
+                case _:
+                    stack.append(log)
+
+        return len(stack)
+
+
