4a5b5fa51618de13a382e306561591f35b72f577
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-13 17:21:06 -0700

    Added Count Good Triplets

---
+++ count_good_triplets.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        result = 0
+        n = len(arr)
+        for i in range(0, n-2):
+            inum = arr[i]
+            for j in range(i+1, n-1):
+                jnum = arr[j]
+                if abs(inum - jnum) <= a:
+                    for k in range(j+1, n):
+                        knum = arr[k]
+                        added = abs(jnum - knum) <= b
+                        added = added and abs(inum - knum) <= c
+                        if added:
+                            result += 1
+
+        return result
---
+++ test_count_good_triplets.py
@@ -0,0 +1,19 @@
+from count_good_triplets import Solution
+
+def test_ex1():
+    arr = [3,0,1,1,9,7]
+    a = 7
+    b = 2
+    c = 3
+    solution = Solution()
+    result = solution.countGoodTriplets(arr, a, b, c)
+    assert result == 4
+
+def test_ex2():
+    arr = [1,1,2,2,3]
+    a = 0
+    b = 0
+    c = 1
+    solution = Solution()
+    result = solution.countGoodTriplets(arr, a, b, c)
+    assert result == 0
