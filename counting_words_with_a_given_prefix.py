45da1992fe9362206a6d3fb76ddc99052444e4bc
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-13 20:06:02 -0700

    Added Counting Words with a Given Prefix

---
+++ counting_words_with_a_given_prefix.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        n = len(pref)
+        result = 0
+        for word in words:
+            m = len(word)
+            if m >= n:
+                if word[:n] == pref:
+                    result += 1
+
+        return result
