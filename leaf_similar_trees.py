c047d7bbb419a3977791a8eec64297b29a2f7676
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-13 17:55:25 -0700

    Added Leaf-Similar Trees

---
+++ leaf_similar_trees.py
@@ -0,0 +1,28 @@
+from tree_node import TreeNode
+from itertools import zip_longest
+from typing import Iterator, Optional
+
+class Solution:
+    def leaf_gen(self, root: Optional[TreeNode]) -> Iterator[int]:
+        def worker(node: Optional[TreeNode]):
+            if node:
+                leaf = node.left is None and node.right is None
+                if leaf:
+                    yield node.val
+                else:
+                    if node.left:
+                        yield from worker(node.left)
+                    if node.right:
+                        yield from worker(node.right)
+        yield from worker(root)
+
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        result = True
+        gen1 = self.leaf_gen(root1)
+        gen2 = self.leaf_gen(root2)
+        for val1, val2 in zip_longest(gen1, gen2):
+            if val1 is None or val2 is None or val1 != val2:
+                result = False
+                break
+
+        return result
---
+++ test_leaf_similar_trees.py
@@ -0,0 +1,22 @@
+from leaf_similar_trees import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    data1 = "[3,5,1,6,2,9,8,null,null,7,4]"
+    data2 = "[3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]"
+    codec = Codec()
+    root1 = codec.deserialize(data1)
+    root2 = codec.deserialize(data2)
+    solution = Solution()
+    result = solution.leafSimilar(root1, root2)
+    assert result
+
+def test_ex2():
+    data1 = "[1,2,3]"
+    data2 = "[1,3,2]"
+    codec = Codec()
+    root1 = codec.deserialize(data1)
+    root2 = codec.deserialize(data2)
+    solution = Solution()
+    result = solution.leafSimilar(root1, root2)
+    assert not result
