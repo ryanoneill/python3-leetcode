bef5bc7a172e8cb294cdf0f02d6b69fb37526d1d
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-16 09:35:41 -0700

    Added Find the Town Judge

---
+++ find_the_town_judge.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trusts = [0] * n
+
+        for pair in trust:
+            truster, trustee = pair
+
+            truster_index = truster - 1
+            trusts[truster_index] -= 1
+            
+            trustee_index = trustee - 1
+            trusts[trustee_index] += 1
+            
+
+        result = -1
+        for i in range(n):
+            if trusts[i] == n-1:
+                result = i + 1
+                break
+                
+        return result
