47ff4acf12afdd5685336697aeaafc6daa7f4eff
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-16 10:07:28 -0700

    Added Matrix Diagonal Sum

---
+++ matrix_diagonal_sum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        m = len(mat)
+
+        result = 0
+        # diagonal
+        for i in range(m):
+            result += mat[i][i]
+
+        # anti-diagonal
+        for i in range(m):
+            j = m-1 - i
+            if i != j:
+                result += mat[i][j]
+
+        return result
---
+++ test_matrix_diagonal_sum.py
@@ -0,0 +1,24 @@
+from matrix_diagonal_sum import Solution
+
+def test_ex1():
+    mat = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+    solution = Solution()
+    result = solution.diagonalSum(mat)
+    assert result == 25 
+
+def test_ex2():
+    mat = [[1,1,1,1],
+          [1,1,1,1],
+          [1,1,1,1],
+          [1,1,1,1]]
+    solution = Solution()
+    result = solution.diagonalSum(mat)
+    assert result == 8
+
+def test_ex3():
+    mat = [[5]]
+    solution = Solution()
+    result = solution.diagonalSum(mat)
+    assert result == 5
