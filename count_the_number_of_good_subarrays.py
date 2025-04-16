5258ce84bf29a3128094c48bf8ddbf841f24ccac
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-16 11:19:16 -0700

    Added Count the Number of Good Subarrays

---
+++ count_the_number_of_good_subarrays.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        counts = Counter()
+        result = 0
+
+        pairs = 0
+        left = 0
+
+        for right, num in enumerate(nums):
+            pairs += counts[num]
+            counts[num] += 1
+
+            while pairs >= k:
+                result += n - right
+                left_num = nums[left]
+                counts[left_num] -= 1
+                pairs -= counts[left_num]
+                left += 1
+
+        return result
