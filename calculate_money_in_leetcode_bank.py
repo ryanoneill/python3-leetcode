5728f48fc0c1a929273ab65e3585733d3c2852ac
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-16 10:32:11 -0700

    Added Calculate Money in Leetcode Bank

---
+++ calculate_money_in_leetcode_bank.py
@@ -0,0 +1,14 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        rem = n % 7
+        weeks = n // 7
+        result = 0
+
+        for i in range(1, weeks+1):
+            result += 28 + (i-1) * 7
+
+        start = weeks + 1
+        for i in range(rem):
+            result += start + i
+
+        return result
---
+++ test_calculate_money_in_leetcode_bank.py
@@ -0,0 +1,19 @@
+from calculate_money_in_leetcode_bank import Solution
+
+def test_ex1():
+    n = 4
+    solution = Solution()
+    result = solution.totalMoney(n)
+    assert result == 10
+
+def test_ex2():
+    n = 10
+    solution = Solution()
+    result = solution.totalMoney(n)
+    assert result == 37
+
+def test_ex3():
+    n = 20
+    solution = Solution()
+    result = solution.totalMoney(n)
+    assert result == 96
