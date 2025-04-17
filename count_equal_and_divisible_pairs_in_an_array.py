8f7158f16239bed14f2fb7c5849d0d8fa879282b
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-16 21:28:37 -0700

    Added Count Equal and Divisible Pairs in an Array

---
+++ count_equal_and_divisible_pairs_in_an_array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        result = 0
+        n = len(nums)
+        indices = {}
+        for i in range(n):
+            num = nums[i]
+            if num in indices:
+                for j in indices[num]:
+                    mult = i * j
+                    if mult % k == 0:
+                        result += 1
+                indices[num].add(i)
+            else:
+                indices[num] = {i}
+
+        return result
---
+++ test_count_equal_and_divisible_pairs_in_an_array.py
@@ -0,0 +1,15 @@
+from count_equal_and_divisible_pairs_in_an_array import Solution
+
+def test_ex1():
+    nums = [3,1,2,2,2,1,3]
+    k = 2
+    solution = Solution()
+    result = solution.countPairs(nums, k)
+    assert result == 4
+
+def test_ex2():
+    nums = [1,2,3,4]
+    k = 1
+    solution = Solution()
+    result = solution.countPairs(nums, k)
+    assert result == 0
