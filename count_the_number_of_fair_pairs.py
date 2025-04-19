f8b23ee7339c440eb2e49b878f39e10a291e0429
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-18 20:00:37 -0700

    Added Count the Number of Fair Pairs

---
+++ count_the_number_of_fair_pairs.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        n = len(nums)
+        nums.sort()
+
+        def worker(value: int) -> int:
+            result = 0
+            left = 0
+            right = n - 1
+
+            while left < right:
+                sum = nums[left] + nums[right]
+                if sum < value:
+                    result += right - left
+                    left += 1
+                else:
+                    right -= 1
+
+            return result
+
+        return worker(upper + 1) - worker(lower)
---
+++ test_count_the_number_of_fair_pairs.py
@@ -0,0 +1,17 @@
+from count_the_number_of_fair_pairs import Solution
+
+def test_ex1():
+    nums = [0,1,7,4,4,5]
+    lower = 3
+    upper = 6
+    solution = Solution()
+    result = solution.countFairPairs(nums, lower, upper)
+    assert result == 6
+
+def test_ex2():
+    nums = [1,7,9,2,5]
+    lower = 11
+    upper = 11
+    solution = Solution()
+    result = solution.countFairPairs(nums, lower, upper)
+    assert result == 1
