4277397d90d86ccac41aa96bdb9573e3a00a9289
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-22 16:53:12 -0700

    Added Count the Number of Ideal Arrays

---
+++ count_the_number_of_ideal_arrays.py
@@ -0,0 +1,28 @@
+import math
+
+class Solution:
+    def idealArrays(self, n: int, maxValue: int) -> int:
+        MOD = 10 ** 9 + 7
+
+        cache = {}
+        def worker(current: int, possibilities: int) -> int:
+            key = (current, possibilities)
+            if key in cache:
+                return cache[key]
+            else:
+                result = math.comb(n-1, possibilities-1)
+                if possibilities < n:
+                    next = current + current
+                    while next <= maxValue:
+                        result += worker(next, possibilities+1)
+                        next += current
+
+                cache[key] = result
+                return result
+
+        result = 0
+        for i in range(1, maxValue+1):
+            result += worker(i, 1)
+        result = result % MOD
+
+        return result
---
+++ test_count_the_number_of_ideal_arrays.py
@@ -0,0 +1,17 @@
+from count_the_number_of_ideal_arrays import Solution
+
+def test_ex1():
+    n = 2
+    maxValue = 5
+    solution = Solution()
+    result = solution.idealArrays(n, maxValue)
+    assert result == 10
+
+
+def test_ex2():
+    n = 5
+    maxValue = 3
+    solution = Solution()
+    result = solution.idealArrays(n, maxValue)
+    assert result == 11
+
