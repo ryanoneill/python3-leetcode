57124bd5f302ca1f9c842a1cbd99f74550b93368
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-23 10:42:56 -0700

    Added Second Minimum Node in a Binary Tree

---
+++ second_minimum_node_in_a_binary_tree.py
@@ -0,0 +1,26 @@
+from tree_node import TreeNode
+from typing import Optional
+
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        def worker(node: Optional[TreeNode], first: int) -> int:
+            result = -1
+            if node:
+                value = node.val
+                if value == first:
+                    left_result = worker(node.left, first)
+                    right_result = worker(node.right, first)
+                    if left_result != -1 and right_result != -1:
+                        result = min(left_result, right_result)
+                    elif left_result != -1:
+                        result = left_result
+                    elif right_result != -1:
+                        result = right_result
+                else:
+                    result = value
+            return result
+
+        result = -1
+        if root:
+            result = worker(root, root.val)
+        return result
---
+++ test_second_minimum_node_in_a_binary_tree.py
@@ -0,0 +1,26 @@
+from second_minimum_node_in_a_binary_tree import Solution
+from serialize_and_deserialize_binary_tree import Codec
+
+def test_ex1():
+    data = "[2,2,5,null,null,5,7]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    result = solution.findSecondMinimumValue(root)
+    assert result == 5
+
+def test_ex2():
+    data = "[2,2,2]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    result = solution.findSecondMinimumValue(root)
+    assert result == -1
+
+def test_ex3():
+    data = "[1,1,3,1,1,3,4,3,1,1,1,3,8,4,8,3,3,1,6,2,1]"
+    codec = Codec()
+    root = codec.deserialize(data)
+    solution = Solution()
+    result = solution.findSecondMinimumValue(root)
+    assert result == 2
