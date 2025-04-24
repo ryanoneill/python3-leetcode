1bfc881ad4f7aa0aaa0eb198cbecf8135651b361
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-24 08:46:20 -0700

    Added Simple Bank System

---
+++ simple_bank_system.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Bank:
+    def __init__(self, balance: List[int]) -> None:
+        self.balances = balance
+        self.accounts = len(balance)
+
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        result = False
+        acc1_index = account1 - 1
+        acc2_index = account2 - 1
+        if 0 <= acc1_index < self.accounts and 0 <= acc2_index < self.accounts:
+            if self.balances[acc1_index] >= money:
+                self.balances[acc1_index] -= money
+                self.balances[acc2_index] += money
+                result = True
+        return result
+
+
+    def deposit(self, account: int, money: int) -> bool:
+        result = False
+        acc_index = account - 1
+        if 0 <= acc_index < self.accounts:
+            self.balances[acc_index] += money
+            result = True
+        return result
+
+
+    def withdraw(self, account: int, money: int) -> bool:
+        result = False
+        acc_index = account - 1
+        if 0 <= acc_index < self.accounts:
+            if self.balances[acc_index] >= money:
+                self.balances[acc_index] -= money
+                result = True
+        return result
+
+
---
+++ test_simple_bank_system.py
@@ -0,0 +1,9 @@
+from simple_bank_system import Bank
+
+def test_ex1():
+    bank = Bank([10, 100, 20, 50, 30])
+    assert bank.withdraw(3, 10)
+    assert bank.transfer(5, 1, 20)
+    assert bank.deposit(5, 20)
+    assert not bank.transfer(3, 4, 15)
+    assert not bank.withdraw(10, 50)
