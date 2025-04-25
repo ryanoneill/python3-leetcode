a5407a92a97f92a3d3607419d7b2d3f3655e4666
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-24 18:54:26 -0700

    Added Count of Interesting Subarrays

---
+++ count_of_interesting_subarrays.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def countInterestingSubarrays(
+        self, nums: List[int], modulo: int, k: int
+    ) -> int:
+        result = 0
+
+        counts = defaultdict(int)
+        counts[0] = 1
+        prefix = 0
+
+        for num in nums:
+            if num % modulo == k:
+                prefix += 1
+
+            add_index = (prefix - k + modulo) % modulo
+            result += counts[add_index]
+            counts[prefix % modulo] += 1
+
+        return result
