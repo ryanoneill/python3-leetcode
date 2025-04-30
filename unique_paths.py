6bbdd4ea34415f52ebd3b0807901de2b8b00def8
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-29 19:00:57 -0700

    Updated Unique Paths to use Less Space

--- unique_paths.py
+++ unique_paths.py
@@ -1,17 +1,22 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        paths = [[0] * n for _ in range(m)]
+        result = 0
+
+        previous_row = [0] * n
+        current_row = [0] * n
 
         for i in range(m):
             for j in range(n):
                 if i == 0:
                     if j == 0:
-                        paths[i][j] = 1
+                        current_row[j] = 1
                     else:
-                        paths[i][j] = paths[i][j - 1]
+                        current_row[j] = current_row[j-1]
                 elif j == 0:
-                    paths[i][j] = paths[i - 1][j]
+                    current_row[j] = previous_row[j]
                 else:
-                    paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
+                    current_row[j] = previous_row[j] + current_row[j-1]
+            result = current_row[n-1]
+            previous_row, current_row = current_row, previous_row
 
-        return paths[m - 1][n - 1]
+        return result
